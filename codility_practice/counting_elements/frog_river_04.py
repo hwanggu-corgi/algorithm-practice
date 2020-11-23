f3cd5ff55b2a3044a4622bdc4bbc778017b2f012
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 07:01:05 +0900

    feat: Add progress 'Frog River'

---
+++ codility_practice/counting_elements/frog_river_04.py
@@ -0,0 +1,24 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# Goal: find the earliest time when the frog can jump to the other side of the river
+#   - The frog can cross only when leaves appear at every position across the river from 1 to X
+#   - If the frog is never able to jump to the other side of the river, the function should return −1.
+
+def solution(X, A):
+    # write your code in Python 3.6
+    checklist = set(range(1, X))
+    N = len(A)
+
+    # iterate through the array
+    while i < N:
+        # check of element from the checklist
+        if A[i] in checklist:
+            checklist.remove(A[i])
+
+        # if the checklist is empty, then return i
+        if len(checklist) == 0:
+            return i
+        i += 1
+
+    return -1
