18a390394fd010cffcb0e41726c270a3f996604b
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 00:47:23 +0900

    feat: Add progress 'binary gap'

--- codility_practice/01.iterations/binary_gap_03.py
+++ codility_practice/01.iterations/binary_gap_03.py
@@ -5,4 +5,12 @@
 
 def solution(N):
     # write your code in Python 3.6
+
+    # convert number to string of binary
+    binary = '{0:08b}'.format(6)
+
+    # count gap
+    # count the number of zeros
+    for bit in binary:
+
     pass
