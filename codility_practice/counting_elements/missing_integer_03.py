7fc96c982921f332fdeee769c780332d5c4ec2e8
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 08:16:34 +0900

    feat: Add progress #3 'Frog River'

---
+++ codility_practice/counting_elements/missing_integer_03.py
@@ -0,0 +1,25 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# The goal is to check whether array A is a permutation.
+
+def solution(A):
+    # write your code in Python 3.6
+
+    N = len(A)
+
+    # create checklist from 1 to N (number of elements in A)
+    # iterate through elements in array
+    i = 0
+    while i < N:
+        # if
+
+        i += 1
+
+
+    # if set is not empty, return false
+    if len(checklist) != 0:
+        return False
+    else:
+        # if set is empty, return true
+        return True
