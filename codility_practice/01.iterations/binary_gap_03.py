0299d13e8e20888c53ff5cee68d677884ea52a80
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 01:05:02 +0900

    feat: Add progress 'binary gap'

--- codility_practice/01.iterations/binary_gap_03.py
+++ codility_practice/01.iterations/binary_gap_03.py
@@ -8,9 +8,20 @@ def solution(N):
 
     # convert number to string of binary
     binary = '{0:08b}'.format(6)
+    print(binary)
+    maximum = 0
+    count = 0
 
     # count gap
-    # count the number of zeros
     for bit in binary:
+        # if 1 exists, check maximum and reset count
+        if (bit == '1'):
+            maximum = max(maximum, count)
+            count = 0
 
-    pass
+        # count the number of zeros
+        if (count == '0'):
+            count += 1
+
+    # return zeros
+    return maximum
