d5951a3a37914d36ce773ea96d7f1da764b0d0c5
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 08:30:16 +0900

    feat: Add progress #4 'Frog River'

--- codility_practice/04.counting_elements/missing_integer_03.py
+++ codility_practice/04.counting_elements/missing_integer_03.py
@@ -7,19 +7,23 @@ def solution(A):
     # write your code in Python 3.6
 
     N = len(A)
-
     # create checklist from 1 to N (number of elements in A)
+    checklist = set(range(1, N+1))
     # iterate through elements in array
     i = 0
     while i < N:
-        # if
+        # if element not in array, then return false
+        if A[i] not in checklist:
+            return 0
+
+        checklist.remove(A[i])
 
         i += 1
 
 
     # if set is not empty, return false
     if len(checklist) != 0:
-        return False
+        return 0
     else:
         # if set is empty, return true
-        return True
+        return 1
