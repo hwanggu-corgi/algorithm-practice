54bde9c8e284307f795f91e900e5312ebe0ac4fb
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 05:10:22 +0900

    feat: Add progress #2 'frog jump'

--- codility_practice/03.time_complexity/frog_jump_04.py
+++ codility_practice/03.time_complexity/frog_jump_04.py
@@ -1,6 +1,12 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# Count the minimal number of jumps that the small frog must perform to reach its target.
+# Frog is currently located at position X
+# Frog jumps fixed distance D
+# Frog wants to get to a position greater than or equal to Y
+
+
 def solution(X, Y, D):
     # write your code in Python 3.6
     pass
