9b58103fe4d43764fa8b847b29c61ba18ef55740
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 00:37:37 +0900

    feat: Add progress 'binary gap'

---
+++ codility_practice/01.iterations/binary_gap_03.py
@@ -0,0 +1,8 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# Goal: return the length of it's longest binary gap
+
+def solution(N):
+    # write your code in Python 3.6
+    pass
