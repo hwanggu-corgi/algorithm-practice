5523568cc5a9b4ad4a8afdd9df4f84b3cdaf41ae
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 02:18:04 +0900

    feat: Add progress #2 'cyclic rotation'

--- codility_practice/02.arrays/cyclic_rotation_04.py
+++ codility_practice/02.arrays/cyclic_rotation_04.py
@@ -1,6 +1,16 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# goal: rotate array A K times; that is, each element of A will be shifted to the right K times
+
 def solution(A, K):
     # write your code in Python 3.6
-    pass
+
+    # find length
+    N = len(A)
+
+    # replace value
+    A[(i+K) % N] = A[i]
+
+
+    return A
