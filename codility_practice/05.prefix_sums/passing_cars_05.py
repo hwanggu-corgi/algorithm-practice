0a0b16b01b01459a24ff0fb8ce05f9c56d905ea6
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-24 08:46:38 +0900

    feat: Add progress 'Passing Cars'

--- codility_practice/05.prefix_sums/passing_cars_05.py
+++ codility_practice/05.prefix_sums/passing_cars_05.py
@@ -1,6 +1,21 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# Goal: count passing cars.
+
 def solution(A):
     # write your code in Python 3.6
-    pass
+    count = 0
+    cars_to_west = sum(A)
+
+    for car in A:
+        # if i is car traveling west, deduct cars_to_west by 1
+        if car == 1:
+            cars_to_west -= 1
+        # if i is car traveling east, then add by number of cars traveling to west from i (cars_to_west)
+        else:
+            count += cars_to_west
+
+    # return count
+
+    return count
