efb3fdebdac89ca0c41fa3324339f2c5cf823953
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 00:31:08 +0900

    feat: Add progress 'Max Slice'

---
+++ codility_practice/09.max_slice/max_slice_03.py
@@ -0,0 +1,13 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# Goal: Given an array A consisting of N integers, returns the maximum sum of any slice of A.
+
+def solution(A):
+    # write your code in Python 3.6
+
+    maximum_sum = sum_ending = 0
+
+    # compute sum ending
+
+    return maximum_sum
