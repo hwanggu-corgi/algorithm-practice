0a1811661a542365c1ea413979cfc51eb51fc186
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 02:17:34 +0900

    feat: Add progress #3 'Tipe Ropes'

--- codility_practice/greedy/tie_ropes_05.py
+++ codility_practice/greedy/tie_ropes_05.py
@@ -4,6 +4,19 @@
 # Goal: given integer K, tie the ropes in such a way that the number of ropes whose
 # length is greater than or equal to K is maximal.
 
+
 def solution(K, A):
     # write your code in Python 3.6
-    pass
+    count = length_ending = 0
+
+    # for each rope, take sum
+    for length in A:
+        # add length to sum
+        length_ending += length
+
+        # Greedy choice: if sum is over K, then reset sum and increment number of ropes length is greater than or equal to K
+        if length_ending >= K:
+            count += 1
+            length_ending = 0
+
+    return count
