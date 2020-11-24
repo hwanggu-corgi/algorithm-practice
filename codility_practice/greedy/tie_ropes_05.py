16854bbd03a6210d190e729839292eecee3fe9f8
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 01:58:27 +0900

    feat: Add progress #2 'Tie Ropes'

--- codility_practice/greedy/tie_ropes_05.py
+++ codility_practice/greedy/tie_ropes_05.py
@@ -1,6 +1,9 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# Goal: given integer K, tie the ropes in such a way that the number of ropes whose
+# length is greater than or equal to K is maximal.
+
 def solution(K, A):
     # write your code in Python 3.6
     pass
