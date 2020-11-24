bc7e977f1eb82141ecc1c51a293bb4843c09a4e2
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 03:45:49 +0900

    feat: Add progress #4 '빛물, BaekJoon'

--- Baekjoon/NHN_고도/빛물.py
+++ Baekjoon/NHN_고도/빛물.py
@@ -13,5 +13,11 @@ def main():
     world_dimensions = [int(x) for x in input().split()]
     block_height = [int(x) for x in input().split()]
 
+    # Find left block
+
+    # If left block exists, find right block
+
+    # If distance between left block and riht block is greater than 1, calculate blocks of water
+
 if __name__ == "__main__":
     main()
