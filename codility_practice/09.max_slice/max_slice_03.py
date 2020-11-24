87357471a2d876ac508e9d33abf1c80a89c67934
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 01:03:15 +0900

    feat: Add progress #3 'Max Slice'

--- codility_practice/09.max_slice/max_slice_03.py
+++ codility_practice/09.max_slice/max_slice_03.py
@@ -7,11 +7,29 @@ def solution(A):
     # write your code in Python 3.6
 
     maximum_sum = sum_ending = 0
+    smallest_negative_number = -1000000
+    neg_number_count = 0
+    N = len(A)
+
+    if N == 0:
+        return 0
+
+    if N == 1:
+        return A[0]
 
     for a in A:
         # compute sum ending
         sum_ending = max(0, sum_ending + a)
         maximum_sum = max(maximum_sum, sum_ending)
 
-        # update su
+        # compute for case when all numbers in A are negative
+        if a < 0:
+            neg_number_count += 1
+            if a > smallest_negative_number:
+                smallest_negative_number = a
+
+
+    if neg_number_count == N:
+        return smallest_negative_number
+
     return maximum_sum
