a589d3826315853a13d6a8453cd27400b4345c1e
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 03:35:29 +0900

    feat: Add progress #3 '빛물, BaekJoon'

--- Baekjoon/NHN_고도/빛물.py
+++ Baekjoon/NHN_고도/빛물.py
@@ -3,7 +3,15 @@
 # - 2차원 세계에서는 한 칸의 용량은 1이다
 # - 빗물이 전혀 고이지 않을 경우 0을 출력하여라
 
+# 입력
+# - 첫 번째 줄 - 2차원 세계의 세로 길이 H과 2차원 세계의 가로 길이
+# - 두 번째 줄 - 블록이 쌓인 높이를 의미하는 0이상 H이하의 정수가 2차원 세계의 맨 왼쪽 위치부터 차례대로 W개 주어진다
+# - 2차원 세계의 바닥은 항상 막혀있다고 가정하여도 좋다.
+
 def main():
-    #
+    # parse input
+    world_dimensions = [int(x) for x in input().split()]
+    block_height = [int(x) for x in input().split()]
+
 if __name__ == "__main__":
     main()
