5a170c109d040c6266b5a212ba78e508af186f51
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 00:12:23 +0900

    feat: Add progress 'Max Profit'

--- codility_practice/09.max_slice/max_profit_04.py
+++ codility_practice/09.max_slice/max_profit_04.py
@@ -8,31 +8,24 @@
 def solution(A):
     # write your code in Python 3.6
 
-    max_profit = profit_ending =  0
-    max_sell_price = purchase_price = A[0]
     N = len(A)
 
-    if len(A) == 0:
+    if N == 0:
         return 0
 
-    if len(A) == 1:
+    if N == 1:
         return 0
+
+    max_profit = profit_ending =  0
+    purchase_price = A[0]
 
     i = 1
     while i < N:
         # calculate profit after selling with price a
         net_price = A[i] - purchase_price
         profit_ending = max(0, profit_ending + net_price)
-
-        # if the profit is positive
-        if profit_ending > 0:
-            # update the max profit
-            max_profit = max(max_profit, profit_ending)
-            purchase_price = A[i]
-        else:
-            # if the profit is negative
-            # reset the purchase price to current
-            purchase_price = A[i]
-
+        # update the max profit
+        max_profit = max(max_profit, profit_ending)
+        purchase_price = A[i]
         i += 1
     return max_profit
