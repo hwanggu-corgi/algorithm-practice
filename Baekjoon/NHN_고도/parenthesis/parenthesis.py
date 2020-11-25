60243b6213048d1f42ff70a8d3a676ab2d73e0ae
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-26 06:10:32 +0900

    feat: Add progress #16 'Parenthesis, BaekJoon'

--- Baekjoon/NHN_고도/parenthesis/parenthesis.py
+++ Baekjoon/NHN_고도/parenthesis/parenthesis.py
@@ -1,8 +1,18 @@
 #Goal:
+#   - Write a program that convert the C expression into the ICPC expression
+#       - Is where pairs of parentheses should be used exactly as needed
+#       -  uses following symbols (+, -, *, /, and %)
+#   - If the expression is not an error in ICPC, then it should not be an error in C
+#   - Once it is not an error in C, the usage of parentheses should be checked to determine whether it is a proper expression in ICPC or not.
+#   - If the expression is not properly parenthesized, i.e., the number of parentheses is not exact as needed, then it is considered improper
 #   - The line should contain one of the following words:
 #       - error
+#           - if the input C expression is erroneous
 #       - proper
+#           - print proper if it is parenthesized properly with the exact number
+#              of parentheses needed for ICPC
 #       - improper.
+#           - print if not proper
 
 def main():
 
