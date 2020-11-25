7b98adc6e525459e1cd1c7f7512ce8ca337dc5fd
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-25 15:18:46 +0900

    feat: Add progress #2 '창고 다각형, BaekJoon'

--- Baekjoon/NHN_고도/창고_다각형.py
+++ Baekjoon/NHN_고도/창고_다각형.py
@@ -1,5 +1,14 @@
-# Goal:
-def main():
+# Goal: 기둥들의 위치와 높이가 주어질 때, 가장 작은 창고 다각형의 면적을 구하는 프로그램을 작성하시오.
+#   - 첫 줄 - 기둥의 개수 N
+#       - N은 1 이상 1,000 이하이다
+#   - N 개의 줄 - 왼쪽 면의 위치를 나타내는 정수 L과 높이를 나타내는 정수 H
+#       - L과 H는 둘 다 1 이상 1,000 이하이다.
 
+def main():
+    # get input
+
+    # sort columns by position on x-axis
+
+    # evaluate smallest area
 if __name__ == "__main__":
     print(main())
