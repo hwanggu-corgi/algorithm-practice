a75b76f6f826543557fe36b2de9510ccd1aceae4
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-26 05:57:30 +0900

    feat: Add progress #15 'Parenthesis, BaekJoon'

---
+++ Baekjoon/NHN_고도/parenthesis/parenthesis.py
@@ -0,0 +1,11 @@
+#Goal:
+#   - The line should contain one of the following words:
+#       - error
+#       - proper
+#       - improper.
+
+def main():
+
+
+if __name__ == "__main__":
+    print(main())
