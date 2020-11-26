284c2c9d63e9f8d5fe5ca51c90dfd61fa6802cd7
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 05:17:53 +0900

    feat: Add progresss #2 '전화번호 목록, programmers'

--- Progammers/전화번호 목록/main.py
+++ Progammers/전화번호 목록/main.py
@@ -1,3 +1,19 @@
+# goal: 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록
+# solution 함수를 작성해주세요.
+#   - phone_book의 길이는 1 이상 1,000,000 이하입니다.
+#   - 각 전화번호의 길이는 1 이상 20 이하입니다.
+
 def solution(phone_book):
+    N = len(phone_book)
+    i = 0
+    while i < N:
+        j = i +1
+        while j < N:
+            if len(phone_book[i]) < len(phone_book[j]) and (phone_book[i] in phone_book[j]):
+                return False
+
+            j += 1
+        i += 1
+
     answer = True
     return answer
