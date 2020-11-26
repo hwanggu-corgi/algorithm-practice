e6df674840fe8e1e970b3e6e3bd5e4462ffe4b68
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 01:45:12 +0900

    feat: Add progress #4 '완전탐색, Programmers'

--- Baekjoon/NHN_고도/완전탐색/main.py
+++ Baekjoon/NHN_고도/완전탐색/main.py
@@ -6,9 +6,7 @@ def solution(answers):
     person_3_answers = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
     i = 0
-    person_1_correct_count = 0
-    person_2_correct_count = 0
-    person_3_correct_count = 0
+    correct_count = [[1,0],[2,0],[3,0]]
     while i < N:
         # get answer for ith question
         correct_answer = answers[i]
@@ -17,30 +15,23 @@ def solution(answers):
         person_1_ans = person_1_answers[i % 5]
         # if is correct, add to count
         if correct_answer == person_1_ans:
-            person_1_correct_count += 1
+            correct_count[0][1] += 1
 
         # get person 2's answer for ith question
         person_2_ans = person_2_answers[i % 8]
         # if is correct, add to count
         if correct_answer == person_2_ans:
-            person_2_correct_count += 1
+            correct_count[1][1] += 1
 
         # get person 2's answer for ith question
         person_3_ans = person_3_answers[i % 10]
         # if is correct, add to count
         if correct_answer == person_3_ans:
-            person_3_correct_count += 1
+            correct_count[2][1] += 1
 
         i += 1
 
     # if all is correct, add number to array
-    if person_1_correct_count > N:
-        answer.append(1)
-
-    if person_2_correct_count == N:
-        answer.append(2)
-
-    if person_3_correct_count == N:
-        answer.append(3)
+    correct_count.sort(key=lambda e: e[1])
 
     return answer
