a39db9753a544ddeb1191f472bcd658f23132cf9
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 03:34:41 +0900

    feat: Add progress #3 '타겟 넘버, Programmers'

--- Baekjoon/NHN_고도/타겟 넘버/main.py
+++ Baekjoon/NHN_고도/타겟 넘버/main.py
@@ -1,12 +1,19 @@
 def solution(numbers, target):
-    answer = _solution(current, target)
+    N = len(numbers)
+    answer = _solution(1, target, 1, N) + _solution(-1, target, 1, N)
     return answer
 
-def _solution(...):
-    # terminal case: when sum == target
+def _solution(value, target, depth, N):
     if depth == N:
         return 1 if value == target else 0
 
-    # travel to the left with current + 1
-    _solution(...)
-    # travel to the right with current - 1
+    count_left = _solution(value + 1, target, depth + 1, N)
+    count_right = _solution(value - 1, target, depth + 1, N)
+
+    return count_left + count_right
+
+if __name__ == "__main__":
+    print(solution([1, 1, 1, 1, 1], 3))
+    print(solution([1], 3))
+    print(solution([1], 1))
+    print(solution([1,1], 1))
