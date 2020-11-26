f7662e65efc68355e63f0f1c8fd2b2cc8173e9bd
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 01:04:49 +0900

    feat: Add progress #2 '완전탐색, Programmers'

--- Baekjoon/NHN_고도/완전탐색/main.py
+++ Baekjoon/NHN_고도/완전탐색/main.py
@@ -1,3 +1,8 @@
 def solution(answers):
     answer = []
+
+    # get person 1's answer for ith question
+    # get answer for ith question
+    # if all is correct, add number to array
+
     return answer
