fcc3182fc62bcba194ef0ee54c156bbd0ce0e757
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 04:17:32 +0900

    feat: Add solution '타겟 넘버, Programmers'

--- Baekjoon/NHN_고도/타겟 넘버/main.py
+++ Baekjoon/NHN_고도/타겟 넘버/main.py
@@ -5,20 +5,15 @@
 # - target: >= 1 and <= 1000
 
 def solution(numbers, target):
-    answer = 0
     answer = _solution(numbers[0], target, 1, numbers) + _solution(-numbers[0], target, 1, numbers)
 
     return answer
 
 def _solution(value, target, index, numbers):
-    if index == (len(numbers) - 1):
-        print("value {}".format(value))
+    if index == len(numbers):
         return 1 if value == target else 0
 
     count_left = _solution(value + numbers[index], target, index + 1, numbers)
     count_right = _solution(value - numbers[index], target, index + 1, numbers)
 
     return count_left + count_right
-
-if __name__ == "__main__":
-    print(solution([1, 1, 1, 1, 1], 3))
