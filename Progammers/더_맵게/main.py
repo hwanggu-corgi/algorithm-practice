a4500e0afd6bb97074daee943fc6027529a5f275
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 08:02:00 +0900

    feat: Add progress #6 '기능개발, programmers'

--- Progammers/더_맵게/main.py
+++ Progammers/더_맵게/main.py
@@ -1,3 +1,15 @@
+# goal: Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때,
+# 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution
+# 함수를 작성해주세요.
+#   - scoville의 길이는 2 이상 1,000,000 이하입니다.
+#   - K는 0 이상 1,000,000,000 이하입니다.
+#   - scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
+#   - 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+
 def solution(scoville, K):
     answer = 0
+
+    # for each scoville in food
+    # check if its below threshold
+
     return answer
