aa1c0086e5339ae4dff7626b8c6bdcd90c513f55
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 00:17:42 +0900

    feat: Add progress '주식가격, Programmers'

--- Baekjoon/NHN_고도/주식가격/main.py
+++ Baekjoon/NHN_고도/주식가격/main.py
@@ -1,5 +1,12 @@
 def solution(prices):
     answer = []
+    N = len(prices)
 
+    i = 0
+    while i < N:
+        # count number of items higher than current value
+        # store count in array answer
+
+        i+=1
 
     return answer
