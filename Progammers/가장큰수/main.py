9987cec09a0e3a9f66c4b7549e955d1af9a52c49
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-28 06:43:02 +0900

    feat: Add progress #9 '가장 큰 수, programmers'

--- Progammers/가장큰수/main.py
+++ Progammers/가장큰수/main.py
@@ -8,7 +8,7 @@ def solution(numbers):
     # convert all number to string
     numbers = [str(x) for x in numbers]
     # sort array
-    numbers.sort(key=lambda e: e + ((4-len(e)) * e[0]))
+    numbers.sort(key=lambda e: e + ((5-len(e)) * e[0]))
 
     # form largest number in string
     i  = len(numbers) - 1
@@ -17,9 +17,11 @@ def solution(numbers):
         i -= 1
     # return result
 
-    return answer
+    return str(int(answer))
 
 if __name__ == "__main__":
+    print(solution([121, 12]))
+    print(solution([0, 0, 1, 10, 1000]))
     print(solution([1, 10, 1000]))
     print(solution([1, 10, 110]))
     print(solution([6, 10, 2]))
