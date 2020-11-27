ea321b556668962a5654eded98e670cf277a30f3
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 09:06:21 +0900

    feat: Add progress #5 '더 맵게, programmers'

--- Progammers/더_맵게/main.py
+++ Progammers/더_맵게/main.py
@@ -7,18 +7,38 @@
 #   - 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
 
 def solution(scoville, K):
-    N = scoville
+    N = len(scoville)
     answer = 0
+
+    scoville = scoville.sort()
 
     i = 1
     # for each scoville in food
     while i < N:
         # if food at i is below threshold, mix food
         if scoville[i-1] < K:
+            new_food_scoville = mix_food(i, scoville)
             # update list with new mixed food (take out 2, add new mixed one)
             # add mixed count
+            scoville.pop(i)
+            scoville.pop(i-1)
+            scoville.append(new_food_scoville)
+            # update N
+            N = len(scoville)
+            count += 1
 
         # update index or break
-        i += 1
+        if N == 1 and scoville[0] < K:
+            return -1
+
+        if scoville[0] >= K:
+            break
 
     return answer
+
+def mix_food(index, scoville):
+    return scoville[index-1] + (scoville[index] * 2)
+
+if __name__ == "__main__":
+    print(solution([1,2,3,4,5], 7))
+    print(solution([1,1], 2))
