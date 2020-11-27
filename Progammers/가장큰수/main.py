10db43f6a7f0f9fc73d309414aa6956985f24c7a
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-28 04:03:53 +0900

    feat: Add progress #2 '가장 큰 수, programmers'

--- Progammers/가장큰수/main.py
+++ Progammers/가장큰수/main.py
@@ -1,3 +1,13 @@
+# Goal: 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+#   - numbers의 길이는 1 이상 100,000 이하입니다.
+#   - numbers의 원소는 0 이상 1,000 이하입니다.
+#   - 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다
+
+# Example
+#  [6, 10, 2]
+
+# Place number
+
 def solution(numbers):
     answer = ''
     return answer
