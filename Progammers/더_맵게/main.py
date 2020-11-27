a77264776f77603f885a41ed87a65967dc292e87
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-27 10:49:46 +0900

    feat: Add progress #9 '더 맵게, programmers'

--- Progammers/더_맵게/main.py
+++ Progammers/더_맵게/main.py
@@ -6,7 +6,6 @@
 #   - scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
 #   - 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
 
-from collections import deque
 
 def solution(scoville, K):
     N = len(scoville)
@@ -14,39 +13,38 @@ def solution(scoville, K):
 
     scoville.sort()
 
-    queue = deque(scoville)
-
     i = 1
     # for each scoville in food
     while i < N:
         # if food at i is below threshold, mix food
-        if queue[i-1] < K:
-            new_food_scoville = mix_food(i, queue)
-            queue.popleft()
+        if scoville[i-1] < K:
+            new_food_scoville = mix_food(i, scoville)
             # update list with new mixed food (take out 2, add new mixed one)
             # add mixed count
+            scoville.pop(0)
             if new_food_scoville <= K:
-                queue[0] = new_food_scoville
+                scoville[0] = new_food_scoville
             else:
-                queue.popleft()
-                queue.append(new_food_scoville)
+                scoville.pop(0)
+                scoville.append(new_food_scoville)
 
             # update N
-            N = len(queue)
+            N = len(scoville)
             answer += 1
+            scoville.sort()
 
         # update index or break
-        if N == 1 and queue[0] <= K:
+        if N == 1 and scoville[0] <= K:
             return -1
 
-        if queue[0] > K:
+        if scoville[0] > K:
             break
 
     return answer
 
-def mix_food(index, queue):
-    lesser_spicy = min(queue[index-1], queue[index])
-    more_spicy = max(queue[index-1], queue[index])
+def mix_food(index, scoville):
+    lesser_spicy = min(scoville[index-1], scoville[index])
+    more_spicy = max(scoville[index-1], scoville[index])
 
     return lesser_spicy + (more_spicy * 2)
 
