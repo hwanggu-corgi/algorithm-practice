2fc5d764a5714aaf532292239b81554e763b83ab
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-28 02:09:36 +0900

    feat: Add progress #4 '입국탐색, programmers'

--- Progammers/입국탐색/main.py
+++ Progammers/입국탐색/main.py
@@ -5,9 +5,24 @@
 #   - 각 심사관이 한 명을 심사하는데 걸리는 시간은 1분 이상 1,000,000,000분 이하입니다.
 #   - 심사관은 1명 이상 100,000명 이하입니다.
 
-# time max - 60 minutes
+# time max - 60 minutes (upper bound)
+# time min - 0 minutes
 #   - 심사관 1: floor(60 / 7) -> 8
 #   - 심시관 2: floor(60 / 10) -> 10
+# More people can be analyzed
+
+# time max - 30 minutes (upper bound)
+# time min - 0 minutes (lower bound)
+#   - 심사관 1: floor(30 / 7) -> 4
+#   - 심사관 2: floor(30 / 10) -> 3
+#   More people can be analyzed
+
+# time max - 15 minutes (upper bound)
+# time min - 0 minutes (lower bound)
+#   - 심사관 1: floor(15 / 7) -> 2
+#   - 심시관 2: floor(15 / 10) -> 1
+
+# time max -
 
 
 def solution(n, times):
