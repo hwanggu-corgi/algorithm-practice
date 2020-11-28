931082211c46bf9248059cd1db2938d53b28b563
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-29 03:03:14 +0900

    feat: Add progress '2016년, programmers'

---
+++ Progammers/2016년/main.py
@@ -0,0 +1,15 @@
+# goal:  두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을
+# 완성하세요
+#   - 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다
+
+# 제한 조건
+#   - 2016년은 윤년입니다.
+#   - 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+
+def solution(a, b):
+    # write number of days per month
+
+    # for each month in a list add number of days
+
+    answer = ''
+    return answer
