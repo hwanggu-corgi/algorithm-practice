38360cbafca3e790951edda6778fa500d67f8dca
Hyungmo Gu <hwanggumedia@gmail.com>
2020-11-29 07:05:26 +0900

    feat: Add progress '키패드 누르기, programmers'

---
+++ Progammers/1_키_패드_누르기/main.py
@@ -0,0 +1,7 @@
+# goal: 순서대로 누를 번호가 담긴 배열 numbers, 왼손잡이인지 오른손잡이인 지를 나타내는 문자열
+# hand가 매개변수로 주어질 때, 각 번호를 누른 엄지손가락이 왼손인 지 오른손인 지를 나타내는 연속된
+# 문자열 형태로 return 하도록 solution 함수를 완성해주세요.
+
+def solution(numbers, hand):
+    answer = ''
+    return answer
