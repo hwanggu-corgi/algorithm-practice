9de3ab283c037c4b1b5c67e441946b2c9d0cb4cd
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-01 01:17:49 +0900

    feat: Add progress #3 '문자열 내 마음대로 정렬하기, programmers'

--- Progammers/2_괄호_변환/main.py
+++ Progammers/2_괄호_변환/main.py
@@ -21,10 +21,23 @@ def solution(p):
     if p == "":
         return p
 
-    # if correctly parenthesized, return result
-    if is_correctly_parenthesized(p):
-        return p
+    answer = _solution(p)
+    return answer
+
+def _solution(p):
+    # w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다
 
     # get closest 균형잡힌 괄호 문자열
-    #
-    return answer
+    # if 문자열 u가 "올바른 괄호 문자열"
+    if correctly_parenthesized(...):
+        # 문자열 v에 대해 1단계부터 다시 수행합니다
+        # 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
+        return u +
+
+    # if not u가 "올바른 괄호 문자열"
+    else:
+        # 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
+        # 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
+        # ')'를 다시 붙입니다.
+        # u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+        # 생성된 문자열을 반환합니다.
