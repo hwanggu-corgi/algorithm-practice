c3789b7703c26bb0a011758b99a468e3178359c8
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-01 04:48:54 +0900

    feat: Add progress #13 '문자열 내 마음대로 정렬하기, programmers'

--- Progammers/2_괄호_변환/main.py
+++ Progammers/2_괄호_변환/main.py
@@ -39,6 +39,7 @@ def _solution(w):
     if correctly_parenthesized(u):
         # 문자열 v에 대해 1단계부터 다시 수행합니다
         # 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
+        print("i am here")
         return u + _solution(v)
 
     # if not u가 "올바른 괄호 문자열"
@@ -49,6 +50,7 @@ def _solution(w):
     # ')'를 다시 붙입니다.
     result += ')'
     # u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+    print("last {}".format(u[1:-1:-1][::-1]))
     result += u[1:-1:-1][::-1]
 
     # 생성된 문자열을 반환합니다.
@@ -109,6 +111,6 @@ class Stack:
         return self.stack_list.pop()
 
 if __name__ == "__main__":
-    print(solution(")(")) #()
-    print(solution("(()())()")) #(()())()
-    # print(solution("()))((()")) #()(())()
+    # print(solution(")(")) #()
+    # print(solution("(()())()")) #(()())()
+    print(solution("()))((()")) #()(())()
