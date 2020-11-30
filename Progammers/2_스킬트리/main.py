a76904a304bedc95a663848c803a40f2ef150833
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-01 06:03:10 +0900

    feat: Add progress '스킬트리, programmers'

---
+++ Progammers/2_스킬트리/main.py
@@ -0,0 +1,6 @@
+# goal: 선행 스킬 순서 skill과 유저들이 만든 스킬트리1를 담은 배열 skill_trees가 매개변수로 주어질
+# 때, 가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
+
+def solution(skill, skill_trees):
+    answer = 0
+    return answer
