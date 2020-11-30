3488b1bfdd9647f99838f9e52067a0f5af353b31
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-01 01:04:07 +0900

    feat: Add progress #2 '문자열 내 마음대로 정렬하기, programmers'

--- Progammers/2_괄호_변환/main.py
+++ Progammers/2_괄호_변환/main.py
@@ -2,7 +2,7 @@
 # 변환한 결과를 return 하도록 solution 함수를 완성해 주세요.
 #   - 다음과 같은 과정을 통해 올바른 괄호 문자열로 변환할 수 있습니다.
 
-#       -1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
+#       -1. x 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
 #       -2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
 #       -3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
 #       -  3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
@@ -16,4 +16,15 @@
 
 def solution(p):
     answer = ''
+
+    # if p is empty, return empty
+    if p == "":
+        return p
+
+    # if correctly parenthesized, return result
+    if is_correctly_parenthesized(p):
+        return p
+
+    # get closest 균형잡힌 괄호 문자열
+    #
     return answer
