0a18c1d148f72459f77f39c6d1081f85f6a4a0a5
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-02 06:40:00 +0900

    feat: Add progress #9 '124 나라의 숫자, programmers'

--- Programmers/2_124 나라의 숫자/main.py
+++ Programmers/2_124 나라의 숫자/main.py
@@ -18,8 +18,19 @@
 #       - 4 -> 11 -> 11
 #       - 5 -> 12 -> 12
 #       - 6 -> 20 -> 14
-#       - 7 -> 21 -> 20
-#       - 8
+#       - 7 -> 21 -> 21
+#       - 8 -> 22 -> 22
+#       - 9 -> 100 -> 24
+#       - 10 -> 101 -> 41
+#       - 11 -> 102 -> 42
+#       - 12 -> 110 -> 44
+#       - 13 -> 111 -> 111
+#       - 14 -> 112 -> 112
+#       - 15 -> 120 -> 114
+#       - 16 -> 121 -> 121
+#       - 17 -> 122 -> 122
+#       - 18 -> 200 -> 124  - first 0 from right becomes 4,
+#       - 18 -> 201 -> 141 - 0 becomes 4 and number after gets subtracted by 1
 
 #   1. 2진법
 #       - 1 -> 1 -> 1
@@ -36,5 +47,8 @@
 #       - 13 -> 1101 -> 111
 
 def solution(n):
-    answer = ''
+    ternary_number = convert_from_decimal_to_ternary(n)
+    strange_number = convert_from_ternary_to_strange(ternary_number)
+
+    answer = strange_number
     return answer
