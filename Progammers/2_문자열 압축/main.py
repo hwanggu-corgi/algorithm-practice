3eda10f32054c04951629c15b2142596678d4bab
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-01 15:49:11 +0900

    feat: Add progress #3 '문자열 압축, programmers'

--- Progammers/2_문자열 압축/main.py
+++ Progammers/2_문자열 압축/main.py
@@ -1,6 +1,16 @@
 # goal: 압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라
 # 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
 
+# 제한사항
+#   - s의 길이는 1 이상 1,000 이하입니다.
+#   - s는 알파벳 소문자로만 이루어져 있습니다.
+
 def solution(s):
     answer = 0
+
+    # while i < N where i represents length of compression string
+    # find the length of string after compression
+    # if length of string is less current, update minimum value
+
+    # return minimum value
     return answer
