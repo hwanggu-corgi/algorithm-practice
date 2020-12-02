b8800abacb97f74b2c74ce1652d45c597a524bcd
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-02 09:47:45 +0900

    feat: Add progress #18 '124 나라의 숫자, programmers'

--- Programmers/2_124 나라의 숫자/main.py
+++ Programmers/2_124 나라의 숫자/main.py
@@ -49,6 +49,10 @@
 from collections import deque
 
 def solution(n):
+
+    if n == 1 or n == 2:
+        return n
+
     ternary_number = convert_from_decimal_to_ternary(n)
     strange_number = convert_from_ternary_to_strange(ternary_number)
 
@@ -70,29 +74,40 @@ def convert_from_ternary_to_strange(ternary_number):
     ternary_number_list = ternary_number.split()
     N = len(ternary_number_list)
 
-    strange_number_list = deque()
+    strange_number_queue = deque()
 
     i = N - 1
     while i >= 0:
         # if ternary_number_list[i+1] == 0
-        if ternary_number_list[i] == "0"
-            # append 4 in strange_number_list
+        if ternary_number_list[i] == "0":
+            # append 4 in strange_number_queue
             if (i == N-1):
-                strange_number_list.append("4")
-            elif ternary_number_list[i+1] != "0":
-                strange_number_list.append("4")
+                strange_number_queue.append("4")
+                i -= 1
+                continue
 
-            if ternary_number_list[i] == "4":
-                strange_number_list.append("2")
-            elif ternary_number_list[i] == "2":
-                strange_number_list.append("1")
-            elif ternary_number_list[i] == "1" and i != 0:
-                strange_number_list.append("4")
+            if ternary_number_list[i+1] != "0":
+                strange_number_queue.appendleft("4")
+                i -= 1
+                continue
+
+            if ternary_number_list[i+1] == "0":
+                if ternary_number_list[i] == "4":
+                    strange_number_queue.appendleft("2")
+                elif ternary_number_list[i] == "2":
+                    strange_number_queue.appendleft("1")
+                elif ternary_number_list[i] == "1" and i != 0:
+                    strange_number_queue.appendleft("4")
 
         else:
-
+            strange_number_queue.appendleft(ternary_number_list[i])
 
         i -= 1
 
+    strange_number = "".join(list(strange_number_queue))
 
     return strange_number
+
+if __name__ == "__main__":
+    print(solution(1))
+    print(solution(3))
