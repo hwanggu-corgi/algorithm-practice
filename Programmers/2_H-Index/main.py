b5f0a163c2199e6582fcb6e2d0bc7d1898f453d7
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-03 04:07:26 +0900

    feat: Add progress #4 'H-index, programmers'

--- Programmers/2_H-Index/main.py
+++ Programmers/2_H-Index/main.py
@@ -6,11 +6,31 @@
 #   - 논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
 
 def solution(citations):
+    N = len(citations)
     answer = 0
 
+    if N == 0:
+        return 0
+
     # sort citations to increasing order
+    citations = sorted(citations, reverse=True)
 
     # find number that has h citations greater than h
+    i = 0
+    while i < N:
+        paper_reference_count = citations[i]
+        number_of_papers_referenced_higher_than_equal_to_i = i + 1
+
+        if ((paper_reference_count >= number_of_papers_referenced_higher_than_equal_to_i) and
+           (paper_reference_count <= number_of_papers_referenced_higher_than_equal_to_i)):
+            answer = paper_reference_count
+            break
+        i += 1
 
     # return answer
     return answer
+
+if __name__ == "__main__":
+    print(solution([])) #0
+    print(solution([1])) #1
+    print(solution([3, 0, 6, 1, 5])) #3
