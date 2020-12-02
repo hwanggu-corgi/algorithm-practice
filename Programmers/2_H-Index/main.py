8bd213e8382f0b2b62c6db36a63b4fcfc51b943e
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-03 02:17:41 +0900

    feat: Add progress #2 'H-index, programmers'

--- Programmers/2_H-Index/main.py
+++ Programmers/2_H-Index/main.py
@@ -1,3 +1,10 @@
+# goal: 어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이
+# 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요
+#
+# 제한 사항
+#   - 과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+#   - 논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+
 def solution(citations):
     answer = 0
     return answer
