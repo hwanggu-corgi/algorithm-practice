62b04e28aaaf327bc8c0a4a188c4c2e767777294
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-05 07:17:07 +0900

    feat: Add progress #4 '큰수 만들기, programmers'

--- Programmers/2_큰_수_만들기/main.py
+++ Programmers/2_큰_수_만들기/main.py
@@ -5,12 +5,17 @@
 #   - number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
 #   - k는 1 이상 number의 자릿수 미만인 자연수입니다.
 
-
+from collections import deque
 
 def solution(number, k):
     answer = ''
 
+    # convert number to queue
+    number_list = [x for x in number]
+
     # find biggest number after removing k
+    current_i = 0
+    while k > 0:
 
     # return result
     return answer
