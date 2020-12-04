36770bece7920eb0ffe49b089c3f91deb6b1c845
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-05 00:31:20 +0900

    feat: Add progress #3 '구멍보트, programmers'

--- Programmers/2_구명보트/main.py
+++ Programmers/2_구명보트/main.py
@@ -27,5 +27,5 @@ def solution(people, limit):
     return answer
 
 if __name__ == "__main__":
-    print(solution([70, 50, 80, 50], 100))
-    print(solution([70, 80, 50], 100))
+    print(solution([70, 50, 80, 50], 100)) #3
+    print(solution([70, 80, 50], 100)) #3
