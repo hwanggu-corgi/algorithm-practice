d10a5e095e325262d435594896794b5be7aedc2a
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-06 00:47:33 +0900

    feat: Add progress '조이스틱, programmers'

---
+++ Programmers/2_조이스틱/main.py
@@ -0,0 +1,10 @@
+# goal: 만들고자 하는 이름 name이 매개변수로 주어질 때, 이름에 대해 조이스틱 조작 횟수의 최솟값을
+# return 하도록 solution 함수를 만드세요
+
+# 제한 사항
+#   - name은 알파벳 대문자로만 이루어져 있습니다.
+#   - name의 길이는 1 이상 20 이하입니다.
+
+def solution(name):
+    answer = 0
+    return answer
