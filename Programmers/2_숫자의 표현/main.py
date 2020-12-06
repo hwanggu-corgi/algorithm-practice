ef05b45de46547424a2651190035a04d9f220c2c
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-07 05:40:49 +0900

    feat: Add progress #2 '숫자의 표현, Programmers'

--- Programmers/2_숫자의 표현/main.py
+++ Programmers/2_숫자의 표현/main.py
@@ -1,3 +1,12 @@
+# goal: 자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는
+# solution를 완성해주세요.
+
+# 제한사항
+# n은 10,000 이하의 자연수 입니다.
+
+# Example
+#   - 15
+
 def solution(n):
     answer = 0
     return answer
