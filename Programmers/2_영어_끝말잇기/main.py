d1f1ceeadc6ddb696a17f9fe1b91a6f717d01873
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-07 01:19:45 +0900

    feat: Add progress '영어 끝말잇기, Programmers'

---
+++ Programmers/2_영어_끝말잇기/main.py
@@ -0,0 +1,11 @@
+# goal: 사람의 수 n과 사람들이 순서대로 말한 단어 words 가 매개변수로 주어질 때, 가장 먼저
+# 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 구해서 return 하도록 solution
+# 함수를 완성해주세요.
+
+def solution(n, words):
+    answer = []
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
