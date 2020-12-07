6bdaa00125a839c8e1b3cd8d5905ef381afec58b
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-08 05:48:27 +0900

    feat: Add progress 'JadenCase 문자열 만들기, Programmers'

--- Programmers/2_JadenCaes_문자열_만들기/main.py
+++ Programmers/2_JadenCaes_문자열_만들기/main.py
@@ -1,3 +1,17 @@
+# goal: 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+# 제한 조건
+# s는 길이 1 이상인 문자열입니다.
+# s는 알파벳과 공백문자(" ")로 이루어져 있습니다.
+# 첫 문자가 영문이 아닐때에는 이어지는 영문은 소문자로 씁니다. ( 첫번째 입출력 예 참고 )
+
+# pseudocode
+#   split s into array of string
+#   for each string,
+#   turn to lowercase letters
+#   find the location of first letter in each string and convert that to uppercase letter
+#   join all letters in the end
+
 def solution(s):
     answer = ''
     return answer
