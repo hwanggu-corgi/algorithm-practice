c425bbab41bded6bff0fae303710d7228ce46a45
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-08 08:12:55 +0900

    feat: Add progress #3 'N개의 최소 공배수, Programmers'

--- Programmers/2_N개의_최소공배수/main.py
+++ Programmers/2_N개의_최소공배수/main.py
@@ -1,5 +1,19 @@
 # goal: n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
 
+# 제한 사항
+#   - arr은 길이 1이상, 15이하인 배열입니다.
+#   - arr의 원소는 100 이하인 자연수입니다.
+
+# Example
+#   1) [2,6,8,14]
+
+# Pseudocode
+#   - Find maximum in array, store in max_num
+#   - in multiples of max_num, check if all divides the number
+#   - if yes, return number
+#   - if not keep going
+
+
 def solution(arr):
     answer = 0
     return answer
