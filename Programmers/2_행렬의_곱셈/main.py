2e1f59f44a5436b9384081f5d90867cc58212d7d
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-08 04:16:42 +0900

    feat: Add progress #4 '행렬의 곱셈, Programmers'

--- Programmers/2_행렬의_곱셈/main.py
+++ Programmers/2_행렬의_곱셈/main.py
@@ -16,8 +16,33 @@
     #       get column length of arr1
     #       for each column in arr1 (m),
     #       compute sum of A[i][m]B[m][j] for all m
-    #       add to list
-    #   after going through all columns in arr2
+    #       add to list new_row
+    #   append new_row to answer
 def solution(arr1, arr2):
-    answer = [[]]
+    answer = []
+    n_row_arr1 = len(arr1)
+    n_col_arr1 = len(arr1[0])
+
+    n_row_arr2 = len(arr2)
+    n_col_arr2 = len(arr2[0])
+
+    #   for each row in arr1 (i),
+    for i in range(n_row_arr1):
+        #   for each column in arr2 (j),
+        new_row = []
+        for j in range(n_col_arr2):
+            new_value = 0
+            #   calculate element after matrix multiplication
+            #   for each column in arr1 (m),
+            for m in range(n_col_arr1):
+                #   compute sum of A[i][m]B[m][j] for all m
+                new_value = arr1[i][m] * arr2[m][j]
+            #   add to list new_row
+            new_row.append(new_value)
+        #   append new_row to answer
+        answer.append(new_row)
     return answer
+
+if __name__ == "__main__":
+    print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]])) # [[15, 15], [15, 15], [15, 15]]
+    print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]])) # [[22, 22, 11], [36, 28, 18], [29, 20, 14]]
