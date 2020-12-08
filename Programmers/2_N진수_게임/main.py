0d94423a8e8f9482d02a33a38c42f84e70091993
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-09 04:01:55 +0900

    feat: Add progress #4 'N 진수 게임, Programmers'

--- Programmers/2_N진수_게임/main.py
+++ Programmers/2_N진수_게임/main.py
@@ -4,10 +4,11 @@
 # Example
 #   n: 2 t: 4 (length of value to print) m: 2 (represents skip distance) p: 1
 #     1) 0 - 0
-#     2) 2 - 11
+#     2) 2 - 10
 #     3) 4 - 100
+#     4) 6 - 110
 
-#     0111 | 00 --> 0111
+#     0111
 
 #   n: 16 t: 16 (length of value to print) m: 2 (represents skip distance) p: 1
 #     1) 0 - 0
@@ -17,7 +18,35 @@
 #     5) 8 - 8
 #     6) 10 - A
 #     7) 12 - C
-#     7) 12 - C
+#     8) 14 - E
+#     9) 16 - 10
+#     10)18 - 12
+#     11)20 - 14
+#     12)22 - 16
+#     13)24 - 18
+#     14)26 - 1A
+#     16)28 - 1C
+
+#     02468ACE1111111
+
+#   n: 16 t: 16 (length of value to print) m: 2 (represents skip distance) p: 2
+#     1) 1 - 1
+#     2) 3 - 3
+#     3) 5 - 5
+#     4) 7 - 7
+#     5) 9 - 9
+#     6) 11 - B
+#     7) 13 - D
+#     8) 15 - F
+#     9) 17 - 11
+#     10)19 - 13
+#     11)21 - 15
+#     12)23 - 17
+#     13)25 - 19
+#     14)27 - 1B
+#     16)29 - 1D
+
+#     13579BDF13579BD
 
 def solution(n, t, m, p):
     answer = ''
