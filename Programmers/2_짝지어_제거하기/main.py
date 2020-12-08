e2d511908fe787fdbd130235e7bdf86a6a1aeebe
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-08 13:47:12 +0900

    feat: Add progress #2 '짝지어 제거하기, Programmers'

--- Programmers/2_짝지어_제거하기/main.py
+++ Programmers/2_짝지어_제거하기/main.py
@@ -4,11 +4,26 @@
 #   문자열의 길이 : 1,000,000이하의 자연수
 #   문자열은 모두 소문자로 이루어져 있습니다.
 
+# Example
+#   1. baabaa
+#       1) bbaa
+#       2) aa
+#       3)      -> return 1
+#
+
+#   2. cdcd
+#       1) cdcd     -> doesn't exist. cannot be removed anymore
+#                   -> return 0
+
+# pseudocode
+#   search for first repeating characters
+#   if it exists, show where it starts
+#       remove characters
+#   if it doesn't exist, return 0
+
 
 def solution(s):
     answer = 0
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
 
     return answer
