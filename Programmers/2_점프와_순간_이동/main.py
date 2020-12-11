025d4eb03b1112ce749337328bc7e77e4054cbb5
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-12 06:51:24 +0900

    feat: Add progress #16 '점프와_순간_이동, Programmers'

--- Programmers/2_점프와_순간_이동/main.py
+++ Programmers/2_점프와_순간_이동/main.py
@@ -38,7 +38,7 @@
 # dp = [0,1,1,2,0,0,0,0] 4 is even --> dp[4] = dp[4 // 2] = 1       4 % 2 = 0 + 1 = 1
 # dp = [0,1,1,2,1,0,0,0] 5 is odd --> dp[5] = dp[5 // 2] + 1 = 2    5 % 2 = 1 + 1 = 2
 # dp = [0,1,1,2,0,0,0,0] 6 is even --> dp[6] = dp[6 // 2] = 2       6 % 2 = 0 + 2 = 2
-# dp = [0,1,1,2,0,0,0,0] 7 is odd --> dp[7] = dp[7 // 2] + 1 = 3    7 % 2 = 1 + 2 = 3
+# dp = [0,1,1,2,0,0,0,0] 7 is odd --> dp[7] = dp[7 // 2] + 1 = 3    7 % 2 = 1 + 2 = 2
 # dp = [0,1,1,2,0,0,0,0] 8 is even --> dp[8] = dp[8 // 2] = 1       8 % 2 = 0 + 1 = 1
 
 # pseudocode
@@ -53,20 +53,29 @@
 #   2. when n != 1
 
 def solution(n):
-    ans = 0
+    count = 0
+    while n != 0:
+        r = n % 2
+        n = n // 2
+        count += r
 
-    #   create array of size n+1 (call it dp)
-    dp = [0] * (n+1)
-    #   for i from 1 until n,
-    i = 1
-    while i < n+1:
-        #   if i is odd, set dp[i] = dp[i//2] + 1
-        dp[i] = dp[i//2] if i %2 == 0 else dp[i//2] + 1
-        #   if i is even, set dp[i] = dp[i//2]
-        i += 1
-
-    ans = dp[-1]
     return ans
+
+# def solution(n):
+#     ans = 0
+
+#     #   create array of size n+1 (call it dp)
+#     dp = [0] * (n+1)
+#     #   for i from 1 until n,
+#     i = 1
+#     while i < n+1:
+#         #   if i is odd, set dp[i] = dp[i//2] + 1
+#         dp[i] = dp[i//2] if i %2 == 0 else dp[i//2] + 1
+#         #   if i is even, set dp[i] = dp[i//2]
+#         i += 1
+
+#     ans = dp[-1]
+#     return ans
 
 if __name__ == "__main__":
     print(solution(5)) #2
