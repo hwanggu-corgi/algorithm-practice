4257ea21a06018909006bb5fb51ef357282d602f
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-11 09:02:32 +0900

    feat: Add progress #2 '파일명 정렬, Programmers'

--- Programmers/2_파일명_정렬/main.py
+++ Programmers/2_파일명_정렬/main.py
@@ -7,6 +7,10 @@
 #   - 각 파일명은 100 글자 이하 길이로, 영문 대소문자, 숫자, 공백(" ), 마침표(.), 빼기 부호(-")만으로 이루어져 있다. 파일명은 영문자로 시작하며, 숫자를 하나 이상 포함하고 있다.
 #   - 중복된 파일명은 없으나, 대소문자나 숫자 앞부분의 0 차이가 있는 경우는 함께 주어질 수 있다. (muzi1.txt, MUZI1.txt, muzi001.txt, muzi1.TXT는 함께 입력으로 주어질 수 있다.)
 
+# Example
+#   1) [img12.png, img10.png, img02.png, img1.png, IMG01.GIF, img2.JPG]
+#      [img1.png, IMG01.GIF, img02.png, img2.JPG, img10.png, img12.png]
+
 def solution(files):
     answer = []
 
