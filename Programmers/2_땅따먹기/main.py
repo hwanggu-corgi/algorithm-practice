7647342a0f01c8a8ea5676d1f12951e03735474e
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-12 13:10:31 +0900

    feat: Add solution '땅따먹기, Programmers'

--- Programmers/2_땅따먹기/main.py
+++ Programmers/2_땅따먹기/main.py
@@ -54,35 +54,47 @@
   #
   # | 5 | 6 | 7 | 100 |    | 10 | 11 | 12 | 11 |
   #
-  # | 4 | 3 | 2 | 1 |      | 16 | 15 | 13 | 13 |
-
-  # | 1 | 2 | 3 | 5 |
-  # |
-
-
+  # | 4 | 3 | 2 | 1 |      | 16 | 15 | 13 | 13 | --> 16
 
 def solution(land):
-    answer = 0
-    prev_column_index = -1
+    N_rows = len(land)
+    N_cols = len(land[0])
+    dp = [[0] * N_cols for x in range(N_rows)]
+    dp[0] = land[0]
 
-    # For each row,
-    for row in land:
-        # crease a new array with element of form [(index, value)].\ (call it row)
-        row_copy = list(zip(range(4), row))
-        # sort array
-        row_copy.sort(key=lambda e: e[1])
-        # check if max value row[-1] is on the same column as before (initially -1)
-        # if yes, choose row[-2], and add to sum
-        # if not, choose row[-1], and add to sum
-        if row_copy[-1][0] == prev_column_index:
-            answer += row_copy[-2][1]
-            prev_column_index = row_copy[-2][0]
-        else:
-            answer += row_copy[-1][1]
-            prev_column_index = row_copy[-1][0]
+    for i in range(1,N_rows):
 
-    # return value
+        dp[i][0] = max(dp[i-1][1], dp[i-1][2], dp[i-1][3]) + land[i][0]
+        dp[i][1] = max(dp[i-1][0], dp[i-1][2], dp[i-1][3]) + land[i][1]
+        dp[i][2] = max(dp[i-1][0], dp[i-1][1], dp[i-1][3]) + land[i][2]
+        dp[i][3] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + land[i][3]
+
+    answer = max (dp[-1])
+
     return answer
+
+# def solution(land):
+#     answer = 0
+#     prev_column_index = -1
+
+#     # For each row,
+#     for row in land:
+#         # crease a new array with element of form [(index, value)].\ (call it row)
+#         row_copy = list(zip(range(4), row))
+#         # sort array
+#         row_copy.sort(key=lambda e: e[1])
+#         # check if max value row[-1] is on the same column as before (initially -1)
+#         # if yes, choose row[-2], and add to sum
+#         # if not, choose row[-1], and add to sum
+#         if row_copy[-1][0] == prev_column_index:
+#             answer += row_copy[-2][1]
+#             prev_column_index = row_copy[-2][0]
+#         else:
+#             answer += row_copy[-1][1]
+#             prev_column_index = row_copy[-1][0]
+
+#     # return value
+#     return answer
 
 if __name__ == "__main__":
     print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]])) #16
