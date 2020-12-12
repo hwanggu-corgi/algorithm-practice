11887b3026dab821cc8da31de18bfb1289a22d2b
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-12 13:34:53 +0900

    feat: Add progress '후보키, Programmers'

---
+++ Programmers/2_ 후보키/main.py
@@ -0,0 +1,18 @@
+# goal: 릴레이션을 나타내는 문자열 배열 relation이 매개변수로 주어질 때, 이 릴레이션에서 후보 키의
+# 개수를 return 하도록 solution 함수를 완성하라.
+
+# 유일성(uniqueness) : 릴레이션에 있는 모든 튜플에 대해 유일하게 식별되어야 한다.
+# 최소성(minimality) : 유일성을 가진 키를 구성하는 속성(Attribute) 중 하나라도 제외하는 경우 유일성이 깨지는 것을 의미한다. 즉, 릴레이션의 모든 튜플을 유일하게 식별하는 데 꼭 필요한 속성들로만 구성되어야 한다.
+
+# 제한사항
+#   - relation은 2차원 문자열 배열이다.
+#   - relation의 컬럼(column)의 길이는 1 이상 8 이하이며, 각각의 컬럼은 릴레이션의 속성을 나타낸다.
+#   - relation의 로우(row)의 길이는 1 이상 20 이하이며, 각각의 로우는 릴레이션의 튜플을 나타낸다.
+#   - relation의 모든 문자열의 길이는 1 이상 8 이하이며, 알파벳 소문자와 숫자로만 이루어져 있다.
+#   - relation의 모든 튜플은 유일하게 식별 가능하다.(즉, 중복되는 튜플은 없다.)
+
+from itertools import permutations
+
+def solution(relation):
+    answer = 0
+    return answer
