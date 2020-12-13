ca217485bbb62f412e331b8d42df8122e0c01a7b
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-14 08:07:15 +0900

    feat: Add progress #2 '가장 큰 정사각형 찾기, Programmers'

--- Programmers/2_가장_큰_정사각형_찾기/main.py
+++ Programmers/2_가장_큰_정사각형_찾기/main.py
@@ -1,6 +1,32 @@
 # goal: 표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return 하는 solution 함수를 완성해
 # 주세요. (단, 정사각형이란 축에 평행한 정사각형을 말합니다.)
 
+# 제한사항
+#   - 표(board)는 2차원 배열로 주어집니다.
+#   - 표(board)의 행(row)의 크기 : 1,000 이하의 자연수
+#   - 표(board)의 열(column)의 크기 : 1,000 이하의 자연수
+#   - 표(board)의 값은 1또는 0으로만 이루어져 있습니다.
+
+
+# Example
+#   1) [[0,0,1,1],[1,1,1,1]]
+#
+#       | 0 | 0 | 1 | 1 | --> choose size 4 (biggest n x n square)
+#       | 1 | 1 | 1 | 1 |
+
+#         x
+#       | 0 | 0 | 1 | 1 | --> i = 0, and there is 0 at j = 0 --> increment i
+#       | 1 | 1 | 1 | 1 |
+
+#             x
+#       | 0 | 0 | 1 | 1 | --> i = 0, and there is 0 at j = 1 --> increment i
+#       | 1 | 1 | 1 | 1 |
+
+#                 x
+#       | 0 | 0 | 1 | 1 | --> i = 0, and its all one !! --> return size 4
+#       | 1 | 1 | 1 | 1 |
+
+
 def solution(board):
     answer = 1234
 
