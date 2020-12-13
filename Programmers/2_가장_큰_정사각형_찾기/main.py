f96bdac091114caf80f18cf4d0818289a508227b
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-14 07:55:13 +0900

    feat: Add progress '가장 큰 정사각형 찾기, Programmers'

---
+++ Programmers/2_가장_큰_정사각형_찾기/main.py
@@ -0,0 +1,10 @@
+# goal: 표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return 하는 solution 함수를 완성해
+# 주세요. (단, 정사각형이란 축에 평행한 정사각형을 말합니다.)
+
+def solution(board):
+    answer = 1234
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
