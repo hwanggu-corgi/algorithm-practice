aae42f6a74c968c14f6f45febf80901e0ef35bd9
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 03:04:12 +0900

    feat: Add progress #3 '채육복, Javascript, Programmers'

--- Programmers/algorithm/1_채육복/solution.js
+++ Programmers/algorithm/1_채육복/solution.js
@@ -1,7 +1,11 @@
 // Pseudocode
 
+// sort lost and reserve
 // find available sets
+    // remove number in both reserve and lost
 // lend reserve to peers
+    // if the different between lost and reserve is 1 apart, lent
+    // if lent, remove number from both lost and reserve
 // return total number of participants in gym class
 
 function solution(n, lost, reserve) {
