b899111bffbbb732327e141bff58139f110c8eb4
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 05:03:40 +0900

    feat: Add progress '2016년, Javascript, Programmers'

---
+++ Programmers/algorithm/1_2016년/solution.js
@@ -0,0 +1,24 @@
+// pseudocode
+
+// find the closest friday to target from january 1st
+
+// find the distance from the closest friday
+// if negative, perform (7 - i)
+
+// print day
+
+function solution(a, b) {
+    let answer = '';
+
+    // pseudocode
+
+    // find the closest friday to target from january 1st
+
+    // find the distance from the closest friday
+    // if negative, perform (7 - i)
+
+    // print day
+    return answer;
+}
+
+console.log(solution(5,24)) // TUE
