18c6f39463d86cc462f3451dbd4ae742ed553dd0
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 07:03:53 +0900

    feat: Add progress #5 '2016년, Javascript, Programmers'

--- Programmers/algorithm/1_2016년/solution.js
+++ Programmers/algorithm/1_2016년/solution.js
@@ -20,38 +20,44 @@
     // daysCount % 7
 // print day
 
-
 function solution(a, b) {
-    let answer = '';
-    let daysCount = 0;
-    let days = {1: "FRI", 2: "SAT", 3:"SUN", 4:"MON", 5:"TUE", 6:"WED", 0:"THU"};
-
-    // find the closest friday to target from january 1st
-    daysCount += get_days(a-1);
-    daysCount += b;
-    // find the distance from the closest friday
-    offset = daysCount % 7;
-
-    answer = days[offset];
+    let days = {0: "SUN", 1: "MON", 2:"TUE", 3:"WED", 4:"THU", 5:"FRI", 6:"SAT"};
+    let today = new Date(`2016-${a}-${b}`).getDay();
+    let answer = days[today];
     return answer;
 }
 
-let get_days = (months) => {
-    let daysCount = 0;
-    let daysInMonth = {
-        1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31,
-        11: 30, 12: 31
-    };
-    if (months == 0) {
-        return 0;
-    }
 
-    for (let i = 1; i <= months; i++) {
-        daysCount += daysInMonth[i];
-    }
+// function solution(a, b) {
+//     let daysCount = 0;
+//     let days = {0: "FRI", 1: "SAT", 2:"SUN", 3:"MON", 4:"TUE", 5:"WED", 6:"THU"};
 
-    return daysCount;
-}
+//     // find the closest friday to target from january 1st
+//     daysCount += get_days(a-1);
+//     daysCount += b;
+//     // find the distance from the closest friday
+//     let offset = (daysCount-1) % 7;
+
+//     let answer = days[offset];
+//     return answer;
+// }
+
+// let get_days = (months) => {
+//     let daysCount = 0;
+//     let daysInMonth = {
+//         1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31,
+//         11: 30, 12: 31
+//     };
+//     if (months == 0) {
+//         return 0;
+//     }
+
+//     for (let i = 1; i <= months; i++) {
+//         daysCount += daysInMonth[i];
+//     }
+
+//     return daysCount;
+// }
 
 console.log(solution(1,1)) // FRI
 console.log(solution(1,8)) // FRI
