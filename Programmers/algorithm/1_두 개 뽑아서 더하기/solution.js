58a5e3382ae4036d5c12278546f069b78c515029
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 00:34:13 +0900

    feat: Add progress #5 '두개 뽑아서 더하기, Javascript, Programmers'

--- Programmers/algorithm/1_두 개 뽑아서 더하기/solution.js
+++ Programmers/algorithm/1_두 개 뽑아서 더하기/solution.js
@@ -7,19 +7,32 @@
 // when it's all done, convert to array and return answer
 
 function solution(numbers) {
-    let N = numbers.length();
-    let answer = [];
+    let N = numbers.length;
+    let numbersSet = new Set();
+
     for (let i = 0; i < N; i++) {
         for (let j = (i+1) ; j < N; j++) {
             // get combinations from numbers
-            number_1 = numbers[i]
-            number_2 = numbers[j]
+            let number_1 = numbers[i];
+            let number_2 = numbers[j];
             // for each combination, add two and push to set
-
-            // when it's all done, convert to array and return answer
+            numbersSet.add(number_1 + number_2);
         }
 
     }
 
+    // when it's all done, convert to array and return answer
+    let answer = Array.from(numbersSet).sort((a,b) => {
+        if (a > b) {
+            return 1;
+        } else if (a == b) {
+            return 0;
+        } else {
+            return - 1;
+        }
+    });
     return answer;
 }
+
+console.log(solution([2,1,3,4,1]));
+console.log(solution([5, 0, 2, 7]));
