31f836671c85d1bc44c29737bfa07887699f8cb2
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 04:18:14 +0900

    feat: Add progress #6 '채육복, Javascript, Programmers'

--- Programmers/algorithm/1_채육복/solution.js
+++ Programmers/algorithm/1_채육복/solution.js
@@ -14,36 +14,47 @@
     // n - (lost.length - lentCount)
 
 function solution(n, lost, reserve) {
-    var answer = 0;
+    let lentCount = 0;
     let N_lost = lost.length;
     // turn lost and reserve to sets
     let lostSet = new Set(lost);
     let reserveSet = new Set(reserve);
+
     // find available sets
     // remove number in both reserve and lost
     lost = lost.filter(x => !(x in reserveSet));
     reserve = reserve.filter(x => !(x in lostSet));
+    reserveSet = new Set(reserve);
 
     // sort lost
+    lost = lost.sort(compare);
     // lend reserve to peers
     // for each lost
-    for (let i = 0; i < N_lost; i++) {
+    console.log(lost);
+    for (i in lost) {
         // if the different between lost and reserve is 1 apart, lend gym shirt
-        // if lent, add count
-        // if lent, remove number from reserve
+        if ((i-1) in reserveSet) {
+            // if lent, add count
+            lentCount += 1;
+
+            // if lent, remove number from reserve
+            reserveSet.remove(i-1);
+        }
     }
 
     // return total number of participants in gym class
-        // n - (lost.length - lentCount)
+    answer = n - (lost.length - lentCount);
     return answer;
 }
 
 function compare(a,b) {
     if (a > b) {
-        return 1;
+        return -1;
     } else if (a == b) {
         return 0;
     } else {
-        return - 1;
+        return 1;
     }
 }
+
+console.log(solution(5, [2,4], [1,3,5])); // 5
