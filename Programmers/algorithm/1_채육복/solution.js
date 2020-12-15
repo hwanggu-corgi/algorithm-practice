878b74d419538b1270ddb543247c8c8bb7640d1a
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 02:54:05 +0900

    feat: Add progress #2 '채육복, Javascript, Programmers'

--- Programmers/algorithm/1_채육복/solution.js
+++ Programmers/algorithm/1_채육복/solution.js
@@ -1,3 +1,9 @@
+// Pseudocode
+
+// find available sets
+// lend reserve to peers
+// return total number of participants in gym class
+
 function solution(n, lost, reserve) {
     var answer = 0;
     return answer;
