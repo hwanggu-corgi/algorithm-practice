2cf7279b649ed5894e11191a8bc91173ff4881eb
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 06:01:04 +0900

    feat: Add progress #2 '2016년, Javascript, Programmers'

--- Programmers/algorithm/1_2016년/solution.js
+++ Programmers/algorithm/1_2016년/solution.js
@@ -1,16 +1,30 @@
+// January - 31 days
+// February - 28 days in a common year and 29 days in leap years
+// March - 31 days
+// April - 30 days
+// May - 31 days
+// June - 30 days
+// July - 31 days
+// August - 31 days
+// September - 30 days
+// October - 31 days
+// November - 30 days
+// December - 31 days
+
 // pseudocode
 
 // find the closest friday to target from january 1st
-
+    // convert month to days (i.e number of days in (5 - 1) months from january) store in days_count
+    // add b to days_count
 // find the distance from the closest friday
+    // days_count % 7
 // if negative, perform (7 - i)
 
 // print day
 
+
 function solution(a, b) {
     let answer = '';
-
-    // pseudocode
 
     // find the closest friday to target from january 1st
 
