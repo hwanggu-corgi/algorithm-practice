1aa24f38718b38b1693dfdd738a560121076cc06
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 00:05:02 +0900

    feat: Add progress #3 '두개 뽑아서 더하기, Javascript, Programmers'

--- Programmers/algorithm/1_두 개 뽑아서 더하기/solution.js
+++ Programmers/algorithm/1_두 개 뽑아서 더하기/solution.js
@@ -1,8 +1,10 @@
-/*
-Pseudocode
 
+// Pseudocode
 
-*/
+// get combinations from numbers
+// for each combination, add two and push to set
+
+// when it's all done, convert to array and return answer
 
 function solution(numbers) {
 
