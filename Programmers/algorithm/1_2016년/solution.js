03f49a3a3fe46ffa24270bb4aab62e973ff6b267
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 06:14:29 +0900

    feat: Add progress #3 '2016년, Javascript, Programmers'

--- Programmers/algorithm/1_2016년/solution.js
+++ Programmers/algorithm/1_2016년/solution.js
@@ -14,25 +14,43 @@
 // pseudocode
 
 // find the closest friday to target from january 1st
-    // convert month to days (i.e number of days in (5 - 1) months from january) store in days_count
-    // add b to days_count
+    // convert month to days (i.e number of days in (5 - 1) months from january) store in daysCount
+    // add b to daysCount
 // find the distance from the closest friday
-    // days_count % 7
-// if negative, perform (7 - i)
-
+    // daysCount % 7
 // print day
 
 
 function solution(a, b) {
     let answer = '';
+    let daysCount = 0;
+    let days = {0: "FRI", 1: "SAT", 2:"SUN", 3:"MON", 4:"TUES", 5:"WED", 6:"THU"};
 
     // find the closest friday to target from january 1st
+    daysCount += get_days(a-1);
 
     // find the distance from the closest friday
-    // if negative, perform (7 - i)
+    offset = daysCount % 7;
 
-    // print day
+    answer = days[offset];
     return answer;
+}
+
+let get_days = (months) => {
+    let daysCount = 0;
+    let daysInMonth = {
+        1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31,
+        11: 30, 12: 31
+    };
+    if (months == 0) {
+        return 0;
+    }
+
+    for (let i = 1; i <= months; i++) {
+        daysCount += daysInMonth[i];
+    }
+
+    return daysCount;
 }
 
 console.log(solution(5,24)) // TUE
