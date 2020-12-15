2bd415cca6834c5962c6c1a5082a8ca9d4bfba38
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 03:57:20 +0900

    feat: Add progress #4 '채육복, Javascript, Programmers'

--- Programmers/algorithm/1_채육복/solution.js
+++ Programmers/algorithm/1_채육복/solution.js
@@ -1,14 +1,46 @@
 // Pseudocode
 
 // sort lost and reserve
+// turn lost and reserve to sets
 // find available sets
     // remove number in both reserve and lost
+// convert lost back to list
 // lend reserve to peers
-    // if the different between lost and reserve is 1 apart, lent
-    // if lent, remove number from both lost and reserve
+// for each lost
+    // if the different between lost and reserve is 1 apart, lend gym shirt
+    // if lent, add count
+    // if lent, remove number from reserve
 // return total number of participants in gym class
+    // n - (lost.length - lentCount)
 
 function solution(n, lost, reserve) {
     var answer = 0;
+    // turn lost and reserve to sets
+    lostSet = new Set(lost)
+    reserveSet = new Set(reserve)
+    // find available sets
+    // remove number in both reserve and
+    lostSet = lostSet - reserveSet;
+    lostSet = lostSet - reserveSet;
+
+    // convert lost back to list
+    // sort lost
+    // lend reserve to peers
+    // for each lost
+        // if the different between lost and reserve is 1 apart, lend gym shirt
+        // if lent, add count
+        // if lent, remove number from reserve
+    // return total number of participants in gym class
+        // n - (lost.length - lentCount)
     return answer;
+}
+
+function compare(a,b) {
+    if (a > b) {
+        return 1;
+    } else if (a == b) {
+        return 0;
+    } else {
+        return - 1;
+    }
 }
