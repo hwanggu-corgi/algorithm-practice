d29aa9650baf54b04a638c8ea33433ad5b49b0ec
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-16 09:06:10 +0900

    feat: Add solution '문자열 내 마음대로 정하기, Javascript, Programmers'

--- Programmers/algorithm/1_문자열_내_마음대로_정렬하기/solution.js
+++ Programmers/algorithm/1_문자열_내_마음대로_정렬하기/solution.js
@@ -1,4 +1,20 @@
 function solution(strings, n) {
-    var answer = [];
+    // sort strings in increasing order
+    strings = strings.sort();
+    // sort strings by nth index
+    strings = strings.sort((a,b) => {
+        if (a[n] > b[n]) {
+            return 1;
+        } else if (a[n] == b[n]) {
+            return 0;
+        } else {
+            return -1;
+        }
+    });
+
+    let answer = strings;
     return answer;
 }
+
+console.log(solution(["sun", "bed", "car"], 1));
+console.log(solution(["abce", "abcd", "cdx"], 2));
