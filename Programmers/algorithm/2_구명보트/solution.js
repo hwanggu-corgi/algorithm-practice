5b617cbbdb26a5aab29c1c48c6c97ee7afcab25e
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-17 06:34:40 +0900

    feat: Add progress #2 '구명보트, Javascript, Programmers'

--- Programmers/algorithm/2_구명보트/solution.js
+++ Programmers/algorithm/2_구명보트/solution.js
@@ -7,5 +7,18 @@
 
 function solution(people, limit) {
     var answer = 0;
+    let N = people.length;
+    let i_left = 0;
+    let i_right = N-1;
+
+    // sort list in order
+    people = people.sort();
+
+    while (i_left < i_right) {
+        // for each large element add small elements until full (no small element exists or small element + large weight >= limit)
+        // when full, add count
+        // repeat until empty
+    }
+
     return answer;
 }
