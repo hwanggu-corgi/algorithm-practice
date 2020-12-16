3bffda08b28dd0bffef25e86bf168a49c74d5046
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-17 06:01:39 +0900

    feat: Add progress '구명보트, Javascript, Programmers'

---
+++ Programmers/algorithm/2_구명보트/solution.js
@@ -0,0 +1,11 @@
+// pseudocode
+
+// sort list in order
+// for each large element add small elements until full (no small element exists or small element + large weight >= limit)
+// when full, add count
+// repeat until empty
+
+function solution(people, limit) {
+    var answer = 0;
+    return answer;
+}
