05095314583d326583cae791d744163d5cdb72bb
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-17 07:02:36 +0900

    feat: Add progress #4 '구명보트, Javascript, Programmers'

--- Programmers/algorithm/2_구명보트/solution.js
+++ Programmers/algorithm/2_구명보트/solution.js
@@ -8,25 +8,30 @@
 function solution(people, limit) {
     var answer = 0;
     let N = people.length;
-    let i_left = 0;
-    let i_right = N-1;
 
     // sort list in order
-    people = people.sort();
-
-    while (i_left < i_right) {
+    people = people.sort(compare);
+    while (i_left <= i_right) {
         let current_weight = people[i_right];
 
         // for each large element add small elements until full (no small element exists or small element + large weight >= limit)
-        while (current_weight < limit && (i_left < i_right)) {
+        while ((current_weight + people[i_left] < limit) && (i_left < i_right)) {
             // when full, add count
             current_weight += people[i_left];
+            console.log(current_weight);
             // repeat until empty
             i_left += 1;
         }
-        count += 1;
-        i_right += 1;
+        answer += 1;
+        i_right -= 1;
     }
 
     return answer;
 }
+
+let compare = (a,b) => {
+    return a - b;
+}
+
+console.log(solution([70, 50, 80, 50], 100)); //3
+console.log(solution([70, 80, 50], 100)); //3
