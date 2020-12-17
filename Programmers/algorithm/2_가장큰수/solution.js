4a30307a54cb2f68634aa973bda105fc216ff005
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-18 03:51:26 +0900

    feat: add progress #2 '가장큰수, Javascript, Programmers'

--- Programmers/algorithm/2_가장큰수/solution.js
+++ Programmers/algorithm/2_가장큰수/solution.js
@@ -1,4 +1,24 @@
+// pseudocode
+//  map element in numbers to string && multiply e by 3
+//  sort elements in string in reverse
+//  reduce elements in array to a single string
+//  return value
+
 function solution(numbers) {
-    var answer = '';
+    //  map element in numbers to string && multiply e by 3
+    numbers = numbers.map(x => String(x) * 3);
+    //  sort elements in string in reverse
+    numbers = numbers.sort((a,b) => {
+        if (a > b) {
+            return -1;
+        } else if (a == b) {
+            return 0;
+        } else {
+            return 1;
+        }
+    });
+    //  reduce elements in array to a single string
+    let answer = numbers.reduce((acc, val) => acc += val, '');
+    //  return value
     return answer;
 }
