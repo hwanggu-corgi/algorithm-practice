7d5ff3ef80adf890f3c7ed051957f0392c108f93
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-17 10:47:57 +0900

    feat: Add solution '위장, Javascript, Programmers'

--- Programmers/algorithm/2_위장/solution.js
+++ Programmers/algorithm/2_위장/solution.js
@@ -2,17 +2,19 @@ function solution(clothes) {
     var answer = 0;
     let count = {};
 
-    for (clothe of clothes) {
-        clothe_type = clothe[1];
+    for (let clothe of clothes) {
+        let clothe_type = clothe[1];
 
-        if (!count.has(clothe_type)) {
+        if (clothe_type in count) {
             count[clothe_type] += 1;
         } else {
             count[clothe_type] = 2; // + 1 from wearing none of the type
         }
     }
 
-    answer = count.values().reduce((acc, val) => {acc *= val;}, 1) - 1; // -1 to remove all nones
+    answer = Object.values(count).reduce((acc, val) => acc *= val, 1) - 1; // -1 to remove all nones
 
     return answer;
 }
+
+console.log(solution(	[["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])); // 5
