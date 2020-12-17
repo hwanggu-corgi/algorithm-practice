60e2d41f937d51d22e0da0a0b0e8c57ce747178d
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-18 00:50:09 +0900

    feat: add progress #3 '다리를 지나는 트럭, Javascript, Programmers'

--- Programmers/algorithm/2_다리를_지나는_트럭/solution.js
+++ Programmers/algorithm/2_다리를_지나는_트럭/solution.js
@@ -10,10 +10,24 @@
 
 // Exmaple
 //  1) bridge_length: 2, weight: 10, truck_weights: [7,4,5,6]
-//  start of i = 0        [0,0] [7,4,5,6]
-//  start of i = 1        [0,7] [4,5,6]
-//  start of i = 2        [7,0] [4,5,6]
-//  start of i = 2        [7,0] [4,5,6]
+//  start of i = 0        [0,0]     [7,4,5,6]
+//  start of i = 1        [0,7]     [4,5,6]     <-- weight limit reached, wait
+//  start of i = 2        [7,0]     [4,5,6]
+//  start of i = 3        [0,4]     [5,6]
+//  start of i = 4        [4,5]     [6]
+//  start of i = 5        [5,0]     [6]
+//  start of i = 6        [0,6]     []
+//  start of i = 7        [6,0]     []
+//  start of i = 8        []        []
+
+// Pseudocode
+//      start from i = 0
+//      while length of truck_weights is greater than 0,
+//      shift bridge
+//      append 0 to bridge
+//      if truck weight is less than bridge weight, push to bridge
+//      else
+//
 
 function solution(bridge_length, weight, truck_weights) {
     var answer = 0;
