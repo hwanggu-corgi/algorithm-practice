f356167e211082569ee0b2dca6108be9fe9e0fe1
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-18 04:18:12 +0900

    feat: add solution '가장큰수, Javascript, Programmers'

--- Programmers/algorithm/2_가장큰수/solution.js
+++ Programmers/algorithm/2_가장큰수/solution.js
@@ -21,7 +21,25 @@ function solution(numbers) {
     //  reduce elements in array to a single string
     let answer = numbers.reduce((acc, val) => acc += val[0], '');
     //  return value
+
+    if (parseInt(answer) == 0) {
+        answer = "0";
+    }
+
     return answer;
 }
 
 console.log(solution([6, 10, 2])); // 6210
+
+console.log(solution([40,403]))
+console.log(solution([10,101]))
+console.log(solution([1,11, 111, 1111]))
+console.log(solution([0,0, 0, 0]))
+console.log(solution([121, 12]))
+console.log(solution([0, 0, 1, 10, 1000]))
+console.log(solution([1, 10, 1000]))
+console.log(solution([1, 10, 110]))
+console.log(solution([6, 10, 2]))
+console.log(solution([6]))
+console.log(solution([0,1]))
+console.log(solution([8,886,884]))
