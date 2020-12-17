6af66202e51ceae795e8c38db46389fe9f08080c
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-17 10:36:44 +0900

    feat: Add progress #2 '위장, Javascript, Programmers'

--- Programmers/algorithm/2_위장/solution.js
+++ Programmers/algorithm/2_위장/solution.js
@@ -1,4 +1,18 @@
 function solution(clothes) {
     var answer = 0;
+    let count = {};
+
+    for (clothe of clothes) {
+        clothe_type = clothe[1];
+
+        if (!count.has(clothe_type)) {
+            count[clothe_type] += 1;
+        } else {
+            count[clothe_type] = 2; // + 1 from wearing none of the type
+        }
+    }
+
+    answer = count.values().reduce((acc, val) => {acc *= val;}, 1) - 1; // -1 to remove all nones
+
     return answer;
 }
