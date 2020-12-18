e53711f011749eb5ac57bbf810922580e18a029e
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-19 06:12:50 +0900

    feat: Add progress '3n 으로 표현, Python, Programmers '

---
+++ Programmers/algorithm/3_n_으로_표현/solution.js
@@ -0,0 +1,10 @@
+// goal: 숫자 N과 number가 주어질 때, N과 사칙연산만 사용해서 표현 할 수 있는 방법 중 N
+// 사용횟수의 최솟값을 return 하도록 solution 함수를 작성하세요.
+
+// Example
+//  1)
+
+function solution(N, number) {
+    var answer = 0;
+    return answer;
+}
