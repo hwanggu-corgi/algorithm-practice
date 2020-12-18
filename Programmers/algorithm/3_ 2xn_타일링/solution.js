f3414e65f84e5d5e82c225b2b95b2b47a4cc2f3b
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-19 00:49:35 +0900

    feat: add progress '2xn 타일링, Javascript, Programmers'

---
+++ Programmers/algorithm/3_ 2xn_타일링/solution.js
@@ -0,0 +1,14 @@
+// goal: 직사각형의 가로의 길이 n이 매개변수로 주어질 때, 이 직사각형을 채우는 방법의 수를 return
+// 하는 solution 함수를 완성해주세요.
+
+// 타일을 가로로 배치 하는 경우
+// 타일을 세로로 배치 하는 경우
+
+
+// Example
+//  1)
+
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
