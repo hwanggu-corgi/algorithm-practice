49e46b4c50d465f6ec73d1e0ce61a723ca50eeaa
Hyungmo Gu <hwanggumedia@gmail.com>
2020-12-18 14:50:13 +0900

    feat: add progress #14 '소수찾기, Javascript, Programmers'

--- Programmers/algorithm/2_소수_찾기/solution.js
+++ Programmers/algorithm/2_소수_찾기/solution.js
@@ -32,10 +32,11 @@ let get_combined_prime_numbers = (number, numbers, n, prime_set) => {
         }
         return;
     }
-    console.log(prime_set);
+
     // else continue to add combination of numbers
     for (const [index, e] of Object.entries(numbers)) {
-        let new_numbers = numbers.substring(0, index) + numbers.substring(index+1);
+        let new_numbers = numbers.split("").splice(index,1).join("");
+        console.log(new_numbers);
         get_combined_prime_numbers(number + e, new_numbers, n, prime_set);
     }
 }
@@ -54,5 +55,9 @@ let is_prime = (number) => {
     return true;
 }
 
+console.log(solution("0")); // 0
+console.log(solution("0000000")); // 0
+console.log(solution("11")); // 1
+console.log(solution("7")); // 1
 console.log(solution("17")); // 3
 console.log(solution("011")); // 2
