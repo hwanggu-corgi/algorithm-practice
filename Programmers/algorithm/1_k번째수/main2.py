801263e5c4b5d301bdc7c08a410f50741787c832
Corgi <hwanggumedia@gmail.com>
2020-12-22 04:17:45 +0900

    feat: Add solution 'K 번째 수, Programmers'

--- Programmers/algorithm/1_k번째수/main2.py
+++ Programmers/algorithm/1_k번째수/main2.py
@@ -6,6 +6,9 @@ def solution(array, commands):
     answer = []
 
     for i,j,k in commands:
-        sub_array = array[i-1:j][k-1].sort()
+        sub_array = sorted(array[i-1:j])
         answer.append(sub_array[k-1])
     return answer
+
+if __name__ =="__main__":
+    print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])) # [5, 6, 3]
