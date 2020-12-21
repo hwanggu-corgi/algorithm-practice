036f57104e52371a6d7171cead0be370edc4f2d4
Corgi <hwanggumedia@gmail.com>
2020-12-21 10:17:18 +0900

    feat: Add progress #2 '네트워크, Javascript, Programmers'

--- Programmers/algorithm/3_네트워크/solution.js
+++ Programmers/algorithm/3_네트워크/solution.js
@@ -1,4 +1,27 @@
 function solution(n, computers) {
-    var answer = 0;
-    return answer;
+    network_count = 0
+    let searched = new Set()
+    let N_rows = computers.length
+
+    for (let computer_id = 0; computer_id < N_rows; computer_id++):
+        if (searched.has(computer_id)) {
+            continue
+        }
+
+        _solution(computer_id, searched, computers)
+
+        network_count += 1
+    return network_count
+}
+
+let _solution = (computer_id, searched, computers) => {
+    if (searched.has(computer_id)) {
+        return;
+    }
+
+    searched.add(computer_id)
+
+    for neighbouring_computer_id in queue:
+        # perform recursion on value
+        _solution(neighbouring_computer_id, searched, computers)
 }
