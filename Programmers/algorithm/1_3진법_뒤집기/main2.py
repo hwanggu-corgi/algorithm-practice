e14b19b56b880aa36ee1253de2607f79783b9d49
Corgi <hwanggumedia@gmail.com>
2020-12-22 07:51:04 +0900

    feat: Add progress '3진법 뒤집기, Programmers'

--- Programmers/algorithm/1_3진법_뒤집기/main2.py
+++ Programmers/algorithm/1_3진법_뒤집기/main2.py
@@ -7,3 +7,27 @@ def solution(n):
     # convert back to decimal
     answer = get_decimal(ternary[::-1])
     return answer
+
+def get_ternary(n):
+    res = ''
+
+    while n != 0:
+        r = n % 3
+        res += str(r) + res
+        n = n // 3
+
+    return res
+
+def get_decimal(ternary):
+    res = 0
+    n = len(ternary) - 1
+
+    while n >= 0:
+        res += 3**n
+        n -= 1
+
+    return res
+
+if __name__ == "__main__":
+    print(solution(45))
+    print(solution(125))
