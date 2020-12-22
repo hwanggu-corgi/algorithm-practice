4f5cc12f56604877fbc4b82d23632f9a9cbb3d3f
Corgi <hwanggumedia@gmail.com>
2020-12-22 13:21:02 +0900

    feat: Add progress '소수 찾기, Programmers'

--- Programmers/algorithm/1_소수_찾기/main.py
+++ Programmers/algorithm/1_소수_찾기/main.py
@@ -2,4 +2,15 @@
 
 def solution(n):
     answer = 0
+    for i in range(2,n+1):
+        answer += 1 if is_prime(i) else 0
     return answer
+
+def is_prime(n):
+    if n == 2:
+        return True
+
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
