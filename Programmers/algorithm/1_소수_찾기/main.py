dedaa8995809e75fa4605fab55ce53c1cd749e7a
Corgi <hwanggumedia@gmail.com>
2020-12-22 15:16:37 +0900

    feat: Add solution '소수 찾기, Programmers'

--- Programmers/algorithm/1_소수_찾기/main.py
+++ Programmers/algorithm/1_소수_찾기/main.py
@@ -2,15 +2,37 @@
 
 def solution(n):
     answer = 0
-    for i in range(2,n+1):
-        answer += 1 if is_prime(i) else 0
-    return answer
+    sieve = [1] * (n+1)
+    sieve[0] = sieve[1] = 0
 
-def is_prime(n):
-    if n == 2:
-        return True
+    i = 2
+    while i * i <= n:
 
-    for i in range(2,n):
-        if n % i == 0:
-            return False
-    return True
+        if sieve[i] == 1:
+            k = i * i
+            while k <= n:
+                sieve[k] = 0
+                k += i
+        i += 1
+
+    return sum(sieve)
+
+
+
+# def solution(n):
+#     answer = 0
+#     for i in range(2,n+1):
+#         answer += 1 if is_prime(i) else 0
+#     return answer
+
+# def is_prime(n):
+#     i = 2
+#     while i * i <= n:
+#         if n % i == 0:
+#             return False
+#         i += 1
+#     return True
+
+if __name__ == "__main__":
+    print(solution(10)) #4
+    print(solution(5)) #3
