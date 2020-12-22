b8f598622d69f387d992c196faecfbd53077d197
Corgi <hwanggumedia@gmail.com>
2020-12-23 00:47:43 +0900

    feat: Add solution '이상한 문자 만들기, Programmers'

--- Programmers/algorithm/1_이상한_문자_만들기/main.py
+++ Programmers/algorithm/1_이상한_문자_만들기/main.py
@@ -1,5 +1,12 @@
 # goal: 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
 
 def solution(s):
-    answer = ''
-    return answer
+    words_list = s.split(" ")
+    answer = []
+
+    for word in words_list:
+        new_word = ""
+        for index, char in enumerate(word):
+            new_word += char.upper() if index % 2 == 0 else char.lower()
+        answer.append(new_word)
+    return " ".join(answer)
