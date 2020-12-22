eb6a526da2113165cf38e871956588f520a48c3a
Corgi <hwanggumedia@gmail.com>
2020-12-23 06:52:38 +0900

    feat: Add solution '핸드폰 번호 가리기, Programmers'

--- Programmers/algorithm/1_하샤드 수/main.py
+++ Programmers/algorithm/1_하샤드 수/main.py
@@ -1,5 +1,4 @@
 # goal: 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요
 
 def solution(x):
-    answer = True
-    return answer
+    return True if x % sum([int(x) for x in str(x)]) == 0 else False
