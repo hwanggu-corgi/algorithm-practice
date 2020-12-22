ce94f81ff5b921eee44c0ebddf441c62f93ab062
Corgi <hwanggumedia@gmail.com>
2020-12-23 00:32:52 +0900

    feat: Add progress '이상한 문자 만들기, Programmers'

--- Programmers/algorithm/1_약수의_합/main.py
+++ Programmers/algorithm/1_약수의_합/main.py
@@ -1,5 +1,4 @@
 # goal: 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
 
 def solution(n):
-    answer = 0
-    return answer
+    return sum([x for x in range(1,n+1) if n % x == 0])
---
+++ Programmers/algorithm/1_이상한_문자_만들기/main.py
@@ -0,0 +1,5 @@
+# goal: 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+def solution(s):
+    answer = ''
+    return answer
