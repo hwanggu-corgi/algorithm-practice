f459bd95d70fabed61bb8bf83ec24a8fb5ef54a5
Corgi <hwanggumedia@gmail.com>
2020-12-22 15:53:25 +0900

    feat: Add progress '시저암호, Programmers'

--- Programmers/algorithm/1_문자열을_정수로_바꾸기/main.py
+++ Programmers/algorithm/1_문자열을_정수로_바꾸기/main.py
@@ -1,5 +1,4 @@
 # goal: 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요
 
 def solution(s):
-    answer = 0
-    return answer
+    return int(s)
---
+++ Programmers/algorithm/1_시저암호/main.py
@@ -0,0 +1,11 @@
+# goal: 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+def solution(s, n):
+    s = [x for x in s]
+
+    for i in s:
+
+
+    answer = "".join()
+
+    return answer
