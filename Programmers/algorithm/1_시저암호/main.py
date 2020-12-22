b57d9b67d6c383cbac4ec462a870f6238d3b4157
Corgi <hwanggumedia@gmail.com>
2020-12-22 16:22:36 +0900

    feat: Add progress #3 '시저암호, Programmers'

--- Programmers/algorithm/1_시저암호/main.py
+++ Programmers/algorithm/1_시저암호/main.py
@@ -6,8 +6,13 @@ def solution(s, n):
 
     i = 0
     while i < n:
-        new_char = ((ord(s[i]) - ord('a')) + n) % 26
-        s[i] = chr(ord('a') + new_char)
+
+        if s[i] = s[i].lower():
+            new_char = ((ord(s[i]) - ord('a')) + n) % 26
+            s[i] = chr(ord('a') + new_char)
+        else:
+            new_char = ((ord(s[i]) - ord('A')) + n) % 26
+            s[i] = chr(ord('A') + new_char)
         i+=1
 
     answer = "".join(s)
