247ea6f319d3c4e378a397b4df8de8c19326f39b
Corgi <hwanggumedia@gmail.com>
2020-12-23 02:15:13 +0900

    feat: Add solution '정수 제곱근 판별, Programmers'

--- Programmers/algorithm/1_정수_제곱근_판별/main.py
+++ Programmers/algorithm/1_정수_제곱근_판별/main.py
@@ -1,5 +1,7 @@
 # goal: n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
 
+import math
+
 def solution(n):
-    answer = 0
-    return answer
+    x =  math.sqrt(n)
+    return (x+1)**2 if x == int(x) else -1
