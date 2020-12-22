8d87cfd4e29c4b18cb891fa51e615d60e6912940
Corgi <hwanggumedia@gmail.com>
2020-12-23 03:57:54 +0900

    feat: Add solution '제일 작은 수 제거하기, Programmers'

--- Programmers/algorithm/1_제일_작은_수_제거하기/main.py
+++ Programmers/algorithm/1_제일_작은_수_제거하기/main.py
@@ -1,3 +1,6 @@
+# goal: 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요
+
 def solution(arr):
-    answer = []
-    return answer
+    min_val = min(arr)
+    arr.remove(min_val)
+    return arr if len(arr) > 0 else [-1]
