a2b2ec8a1514e8260dd4456a0c34ecd171c09536
Corgi <hwanggumedia@gmail.com>
2020-12-24 02:47:02 +0900

    feat: Add progress 'Count Factors, Codility'

---
+++ codility_practice/prime_and_composite_numbers/count_factors.py
@@ -0,0 +1,13 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# cases
+
+def solution(N):
+    # write your code in Python 3.6
+    factors = 0
+
+    i = 2
+    while i * i <= n:
+
+    pass
