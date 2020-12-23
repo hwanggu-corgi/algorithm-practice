d38ff3796499732b4e073233a0ae928e390fcb6b
Corgi <hwanggumedia@gmail.com>
2020-12-23 09:23:25 +0900

    feat: Add progress 'Flags, Codility'

---
+++ codility_practice/prime_and_composite_numbers/flags.py
@@ -0,0 +1,21 @@
+# goal: The goal is to set the maximum number of flags on the peaks, according to certain rules.
+
+# Example
+#   1)
+#       A[0] = 1
+#       A[1] = 5    <- peak 1
+#       A[2] = 3
+#       A[3] = 4    <- peak 2
+#       A[4] = 3
+#       A[5] = 4    <- peak 3
+#       A[6] = 1
+#       A[7] = 2
+#       A[8] = 3
+#       A[9] = 4
+#       A[10] = 6   <- peak 4
+#       A[11] = 2
+
+def solution(A):
+    # write your code in Python 3.6
+
+    pass
