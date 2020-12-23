039ff16dadcac6dd9c85dcd08969268299da94f7
Corgi <hwanggumedia@gmail.com>
2020-12-24 07:02:31 +0900

    feat: Add progress #2 'Minimum Perimeter Rectangle, Codility'

--- codility_practice/prime_and_composite_numbers/min_perimeter_rectangle.py
+++ codility_practice/prime_and_composite_numbers/min_perimeter_rectangle.py
@@ -1,6 +1,24 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# pseudocode
+#   start i from 1
+#   find other value x satisfying i * x = N
+#   evaluate perimeter
+#   store val in min_perimeter = min(min_perimeter, perimeter)
+import sys
 def solution(N):
     # write your code in Python 3.6
-    pass
+    min_perimeter = sys.maxsize
+    a = 1
+    while a * a <= N:
+        b = N / a
+        perimeter = (a + b) * 2
+        min_perimeter = min(int(perimeter), min_perimeter)
+        a += 1
+
+    return min_perimeter
+
+if __name__ == "__main__":
+    print(solution(30)) # 22
+    print(solution(1)) # 1
