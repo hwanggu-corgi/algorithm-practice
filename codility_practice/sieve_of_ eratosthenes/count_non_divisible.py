c1ae73f8a00cf434c577ae74d92f79fe32305c89
Corgi <hwanggumedia@gmail.com>
2020-12-24 08:00:38 +0900

    feat: Add progress 'Count Non Divisible, Codility'

--- codility_practice/sieve_of_ eratosthenes/count_non_divisible.py
+++ codility_practice/sieve_of_ eratosthenes/count_non_divisible.py
@@ -2,5 +2,16 @@
 # print("this is a debug message")
 
 def solution(A):
+    answer = []
     # write your code in Python 3.6
-    pass
+    for index_a, a in enumerate(A):
+        count = 0
+        for index_b, b in enumerate(A):
+
+            if index_a == index_b:
+                continue
+
+            if a % b != 0:
+                count += 1
+        answer.append(count)
+    return answer
