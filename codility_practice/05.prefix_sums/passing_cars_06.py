e9bfedbfbefcaea8004627c466d9a238223d5ae5
Corgi <hwanggumedia@gmail.com>
2020-12-24 01:17:46 +0900

    feat: Add progress #2 'passing cars, Codility'

--- codility_practice/05.prefix_sums/passing_cars_06.py
+++ codility_practice/05.prefix_sums/passing_cars_06.py
@@ -1,3 +1,14 @@
 def solution(A):
     # write your code in Python 3.6
-    pass
+    total = 0
+    cars_to_west = sum(A)
+
+    if cars_to_west == 0:
+        return 0
+
+    for index, car in enumerate(A):
+        if car == 0:
+            total += sum(A[index:])
+            cars_to_west -= 1
+
+    return total
