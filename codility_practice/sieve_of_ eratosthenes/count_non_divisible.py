c717fa3505ce02674121e5784729743d00340837
Corgi <hwanggumedia@gmail.com>
2020-12-24 10:15:04 +0900

    feat: Add progress #3 'Count Non Divisible, Codility'

--- codility_practice/sieve_of_ eratosthenes/count_non_divisible.py
+++ codility_practice/sieve_of_ eratosthenes/count_non_divisible.py
@@ -1,24 +1,23 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-from collections import Counter
+# count elements in A store in dictionary count
+
+# for each element in A,
+# for each key in count,
+# if element % key == 0 --> continue
+# else
+#   if element == key and count[key] > 0, add count[key] - 1 to total
+#   if element != key count[key] to total
+
+# write your code in Python 3.6
 
 def solution(A):
     answer = []
-    # count elements in A store in dictionary count
-
-    # for each element in A,
-    # for each key in count,
-    # if element % key == 0 --> continue
-    # else
-    #   if element == key and count[key] > 0, add count[key] - 1 to total
-    #   if element != key count[key] to total
-
-    # write your code in Python 3.6
-    count = {}
+    e_count = {}
 
     for e in A:
-        if e not in count:
+        if e not in e_count:
             e_count[e] = 1
         else:
             e_count[e] += 1
@@ -26,11 +25,15 @@ def solution(A):
 
     for a in A:
         non_divisors_count = 0
-        for key in count:
-            if a % b == 0:
+        for key in e_count:
+            if a % key == 0:
                 continue
 
             non_divisors_count += e_count[key]
 
         answer.append(non_divisors_count)
     return answer
+
+
+if __name__ == "__main__":
+    print(solution([3, 1, 2, 3, 6])) #
