20f4b13c90512d28e022f686a2a343c4fe15d7be
Corgi <hwanggumedia@gmail.com>
2020-12-25 03:02:17 +0900

    feat: Add progress #2 'Chocolates by Numbers, Codility'

--- codility_practice/12.euclidean_algorithm/chocolates_by_numbers.py
+++ codility_practice/12.euclidean_algorithm/chocolates_by_numbers.py
@@ -1,6 +1,21 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+# goal: The goal is to count the number of chocolates that you will eat, following
+# the above rules.
+
+# - You begin with eating chocolate number 0.
+# -  if you ate chocolate number X, then you will next eat the chocolate with number
+# (X + M) modulo N (remainder of division).
+# - You stop eating when you encounter an empty wrapper.
+
+# Example
+#   1) N = 10   M = 4
+#       0, 4, 8, 2 ,6
+
+# Pseudocode
+# while chocolate to eat is not in set
+# add chocolate to set
+# shift chocolate by M
 
 def solution(N, M):
     # write your code in Python 3.6
+
     pass
