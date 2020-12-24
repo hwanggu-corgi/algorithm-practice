16800df566fa8d91a65b871c55ccfff1cdda02db
Corgi <hwanggumedia@gmail.com>
2020-12-25 07:25:08 +0900

    feat: Add progress #9 'Prime Divisors, Codility'

--- codility_practice/12.euclidean_algorithm/prime_divisors.py
+++ codility_practice/12.euclidean_algorithm/prime_divisors.py
@@ -1,23 +1,6 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-# pseudocode
-# get gcd of A[i] and B[i]
-# divide A[i] and B[i] by gcd
-# find if prime divisor exists in A[i], if so skip i
-# find if prime divisor exists in B[i], if so skip i
-# if all is well, return i
-
-
-# pseudocode
-# for each divisor in A
-# check if divisor is prime
-# if prime, add to set
-
-# for each divisor in B
-# check if divisor is in set, if so remove divisor from set
-# if not in set, return
-
 def solution(A, B):
     n = len(A)
     i = 0
