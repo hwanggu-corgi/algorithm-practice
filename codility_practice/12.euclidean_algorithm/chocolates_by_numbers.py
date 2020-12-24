587b3ccc74ea7582a33d166365d8e8c081e96ad3
Corgi <hwanggumedia@gmail.com>
2020-12-25 03:32:09 +0900

    feat: Add solution 'Chocolates by Numbers, Codility'

--- codility_practice/12.euclidean_algorithm/chocolates_by_numbers.py
+++ codility_practice/12.euclidean_algorithm/chocolates_by_numbers.py
@@ -10,22 +10,48 @@
 #   1) N = 10   M = 4
 #       0, 4, 8, 2 ,6
 
+# find gcd of the two
+# divide N by gcd
+
+def solution(N, M):
+    # find gcd of the two
+    bigger = max(N,M)
+    smaller = min(N,M)
+
+    gcd_NM = gcd(bigger, smaller)
+
+    # divide N by gcd
+    answer = N // gcd_NM
+
+    return answer
+
+def gcd(a,b):
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
 # Pseudocode
 # while chocolate to eat is not in set
 # add chocolate to set
 # shift chocolate by M
 
-def solution(N, M):
-    # write your code in Python 3.6
+# def solution(N, M):
+#     # write your code in Python 3.6
 
-    chocolates_eaten_set = set()
-    current_chocolate = 0
-    # while chocolate to eat is not in set
-    while not current_chocolate in chocolates_eaten_set:
+#     chocolates_eaten_set = set()
+#     current_chocolate = 0
+#     # while chocolate to eat is not in set
+#     while not current_chocolate in chocolates_eaten_set:
 
-        # add chocolate to set
-        chocolates_eaten_set.add(current_chocolate)
-        # shift chocolate by M
-        current_chocolate = (current_chocolate + M) % N
+#         # add chocolate to set
+#         chocolates_eaten_set.add(current_chocolate)
+#         # shift chocolate by M
+#         current_chocolate = (current_chocolate + M) % N
 
-    return len(chocolates_eaten_set)
+#     return len(chocolates_eaten_set)
+
+
+if __name__ == "__main__":
+    print(solution(20, 4)) # 5
+    print(solution(10, 3)) # 10
