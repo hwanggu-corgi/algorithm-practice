ce2e3df300741e201d7b2525933125093dc98d87
Corgi <hwanggumedia@gmail.com>
2020-12-25 01:19:19 +0900

    feat: Add solution 'Max Profit, Codility'

--- codility_practice/09.max_slice/max_profit_05.py
+++ codility_practice/09.max_slice/max_profit_05.py
@@ -5,13 +5,22 @@ def solution(A):
     max_ending = 0
     max_profit = -sys.maxsize
     # subtract A[i] and A[i-1] where i starts from 1
+
+    if n == 1 or n == 0:
+        return 0
+
     i = 1
     while i < n:
         # update max_ending
-        a = A[i]
-        max_ending = max(0, max_ending + a)
+        revenue = A[i] - A[i-1]
+        max_ending = max(0, max_ending + revenue)
         # update max_profit
         max_profit = max(max_profit, max_ending)
         i += 1
 
     return max_profit
+
+if __name__ == "__main__":
+    print(solution([0]))
+    print(solution([1,2]))
+    print(solution([2,1]))
