39f8b797726da209e4661db945c5f25ccc1b68f2
Corgi <hwanggumedia@gmail.com>
2020-12-25 01:02:40 +0900

    feat: Add progress #2 'Max Profit, Codility'

--- codility_practice/09.max_slice/max_profit_05.py
+++ codility_practice/09.max_slice/max_profit_05.py
@@ -1,3 +1,17 @@
+import sys
 def solution(A):
     # write your code in Python 3.6
-    pass
+    n = len(A)
+    max_ending = 0
+    max_profit = -sys.maxsize
+    # subtract A[i] and A[i-1] where i starts from 1
+    i = 1
+    while i < n:
+        # update max_ending
+        a = A[i]
+        max_ending = max(0, max_ending + a)
+        # update max_profit
+        max_profit = max(max_profit, max_ending)
+        i += 1
+
+    return max_profit
