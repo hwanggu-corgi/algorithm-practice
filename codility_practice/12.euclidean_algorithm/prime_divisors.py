b0f7652c505cb517ee20b80f9f24ca8ef3a03f8b
Corgi <hwanggumedia@gmail.com>
2020-12-25 03:48:08 +0900

    feat: Add progress 'Prime Divisors, Codility'

---
+++ codility_practice/12.euclidean_algorithm/prime_divisors.py
@@ -0,0 +1,12 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# pseudocode
+# find smaller number of A and B
+# for each divisor in A
+# check if divisor is prime
+# check if divisor also divides B
+
+def solution(A, B):
+    # write your code in Python 3.6
+    pass
