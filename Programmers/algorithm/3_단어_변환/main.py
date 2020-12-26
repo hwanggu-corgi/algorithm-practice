a7ee12369706d11b995bf4103229f8833bb0f88a
Corgi <hwanggumedia@gmail.com>
2020-12-26 15:37:14 +0900

    feat: Add progress '단어 변환, Programmers'

---
+++ Programmers/algorithm/3_단어_변환/main.py
@@ -0,0 +1,6 @@
+# goal:두 개의 단어 begin, target과 단어의 집합 words가 매개변수로 주어질 때, 최소 몇 단계의
+# 과정을 거쳐 begin을 target으로 변환할 수 있는지 return 하도록 solution 함수를 작성해주세요.
+
+def solution(begin, target, words):
+    answer = 0
+    return answer
