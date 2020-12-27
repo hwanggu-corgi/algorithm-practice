4f372f2a5d71aa7adf25084b75b6e130c0d8d70e
Corgi <hwanggumedia@gmail.com>
2020-12-27 09:43:05 +0900

    feat: Add progress #3 '섬 연결하기, Programmers'

--- Programmers/algorithm/3_섬_연결하기/main.py
+++ Programmers/algorithm/3_섬_연결하기/main.py
@@ -12,6 +12,7 @@
 
 # Example
 #   1) n - 4 costs - [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
+#       - choose minimum value for all incoming connections to node x
 
 def solution(n, costs):
     answer = 0
