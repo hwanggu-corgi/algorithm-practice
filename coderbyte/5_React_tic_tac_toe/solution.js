9d8c5cebf7429d92fc490bdf25d23c6af8efd864
Corgi <hwanggumedia@gmail.com>
2020-12-30 07:20:59 +0900

    feat: Add progress #9 'React Tic Tac Toe, Coderbyte'

--- coderbyte/5_React_tic_tac_toe/solution.js
+++ coderbyte/5_React_tic_tac_toe/solution.js
@@ -91,7 +91,42 @@ class Board extends React.Component {
       state.board[i][j] = turn == 1 ? 'X' : 'O';
       return state;
     });
+  }
 
+  check = (board) => {
+
+    // horizontal check
+    for (let i = 0; i < 3; i++) {
+      if (((board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) &&
+          ((board[i][0] != '') && (board[i][1] != '') && board[i][2] != '')) {
+        return true;
+      }
+    }
+
+    // vertical check
+    for (let j = 0; j < 3; j++) {
+      if (((board[0][j] == board[1][j]) && (board[1][j] == board[2][j])) &&
+          ((board[0][j] != '') && (board[1][j] != '') && board[2][j] != '')) {
+        return true;
+      }
+    }
+
+    // cross check
+    if (((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) &&
+        (board[0][0] != '')) {
+        return true;
+    }
+
+
+    if (((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) &&
+        (board[0][2] != '')) {
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
     if (this.check(this.state.board)) {
       this.setState((state, props) => {
         state.winner = turn;
@@ -100,39 +135,10 @@ class Board extends React.Component {
     }
 
     this.setState((state, props) => {
-      state.turn = 1 ? state.turn == 2 : 1;
+      state.turn = turn == 2 ? 1 : 2;
       return state;
     });
-  }
 
-  check = (board) => {
-
-    // horizontal check
-    for (let i = 0; i < 3; i++) {
-      if (((board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) &&
-          (board[i][0] != '')) {
-        return true;
-      }
-    }
-
-    // vertical check
-    for (let j = 0; j < 3; j++) {
-      if (((board[0][j] == board[1][j]) && (board[1][j] == board[2][j])) &&
-          (board[0][j] != '')) {
-        return true;
-      }
-    }
-
-    // cross check
-    if (((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) ||
-        ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]))) {
-      return true;
-    }
-
-    return false;
-  }
-
-  render() {
     return (
       <div style={containerStyle} className="gameBoard">
         <div className="status" style={instructionsStyle}>Next player: {this.state.turn}</div>
