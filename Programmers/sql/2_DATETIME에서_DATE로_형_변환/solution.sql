2493b413eba6c4f293660e0afacc813ae9cc4cdb
Corgi <hwanggumedia@gmail.com>
2021-01-04 06:54:45 +0900

    feat: Add solution 'Datetime에서 Date로 형 변환, Programmers'

--- Programmers/sql/2_DATETIME에서_DATE로_형_변환/solution.sql
+++ Programmers/sql/2_DATETIME에서_DATE로_형_변환/solution.sql
@@ -1,7 +1,6 @@
 -- Reference: https://stackoverflow.com/questions/4740612/query-to-convert-from-datetime-to-date-mysql
--- SELECT
---     CONVERT(DATE, GETDATE()) date;
+-- SELECT DATE_FORMAT('2004-01-20' ,'%Y-%m-01');
 
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, DATE(DATETIME) AS "날짜"
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, "%Y-%m-%d") AS "날짜"
     FROM ANIMAL_INS;
