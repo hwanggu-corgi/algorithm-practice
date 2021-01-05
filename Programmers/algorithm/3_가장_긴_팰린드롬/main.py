615d2cc7d182c27d676f78c084a67fc9b9557638
Corgi <hwanggumedia@gmail.com>
2021-01-06 00:40:25 +0900

    feat: Add progress #2 '가장 긴 팰린드롬, programmers'

--- Programmers/algorithm/3_가장_긴_팰린드롬/main.py
+++ Programmers/algorithm/3_가장_긴_팰린드롬/main.py
@@ -1,9 +1,13 @@
 # goal: 문자열 s가 주어질 때, s의 부분문자열(Substring)중 가장 긴 팰린드롬의 길이를 return 하는 solution 함수를 완성해 주세요.
 
+# 제한사항
+#   문자열 s의 길이 : 2,500 이하의 자연수
+#   문자열 s는 알파벳 소문자로만 구성
+
 def solution(s):
-answer = 0
+    answer = 0
 
-# [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-print('Hello Python')
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
 
-return answer
+    return answer
