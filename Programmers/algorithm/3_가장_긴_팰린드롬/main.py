c1f14ad8a77d7ce41f6a0809483903c5516026d7
Corgi <hwanggumedia@gmail.com>
2021-01-05 16:01:28 +0900

    feat: Add progress '가장 긴 팰린드롬, programmers'

---
+++ Programmers/algorithm/3_가장_긴_팰린드롬/main.py
@@ -0,0 +1,9 @@
+# goal: 문자열 s가 주어질 때, s의 부분문자열(Substring)중 가장 긴 팰린드롬의 길이를 return 하는 solution 함수를 완성해 주세요.
+
+def solution(s):
+answer = 0
+
+# [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print('Hello Python')
+
+return answer
