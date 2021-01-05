728b3a23698613034aca6e2161f8d6cc9d20453a
Corgi <hwanggumedia@gmail.com>
2021-01-05 13:14:13 +0900

    feat: Add progress #36 '순위, programmers'

--- Programmers/algorithm/3_순위/main.py
+++ Programmers/algorithm/3_순위/main.py
@@ -105,4 +105,4 @@ def generate_dp(n, results):
     return res
 if __name__ == "__main__":
     print(solution(5, [[3, 4], [2, 4], [2, 3], [2, 1], [5, 2]])) # 2
-    print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]])) # 2
+    # print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]])) # 2
