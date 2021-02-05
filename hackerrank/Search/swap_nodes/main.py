3f5d26ff70723df51033fc50f90008ae83da88c8
Corgi <hwanggumedia@gmail.com>
2021-02-06 06:46:20 +0900

    feat: Add progress 'Swap Nodes, HackerRank'

---
+++ hackerrank/Search/swap_nodes/main.py
@@ -0,0 +1,9 @@
+# Goal:
+#  Complete the swapNodes function in the editor below. It should return a
+#  two-dimensional array where each element is an array of integers representing
+#  the node indices of an in-order traversal after a swap operation.
+
+def swapNodes(indexes, queries):
+    #
+    # Write your code here.
+    #
