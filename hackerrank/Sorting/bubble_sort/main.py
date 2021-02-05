0df71fc2f14f242c9e1ce84e1387f21ffbc3994c
Corgi <hwanggumedia@gmail.com>
2021-02-06 02:02:57 +0900

    feat: Add progress #2 'Bubble Sort, HackerRank'

--- hackerrank/Sorting/bubble_sort/main.py
+++ hackerrank/Sorting/bubble_sort/main.py
@@ -5,10 +5,19 @@
 
 
 def countSwaps(a):
+    n = len(a)
+    for i in range(n):
 
-
+        for j in range(n-1) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1], a);
+            }
+        }
 
     print("Array is sorted in {} swaps.".format(count))
     print("First Element: {}".format(a[0]))
     print("Last Element: {}".format(a[-1]))
+
+
+def swap(a_1, a_2, a):
 
