53f23be6d57cf147ecaba7d9e70c76cb859c48ef
Corgi <hwanggumedia@gmail.com>
2021-02-06 07:04:34 +0900

    feat: Add progress #2 'Swap Notes, HackerRank'

--- hackerrank/Search/swap_nodes/main.py
+++ hackerrank/Search/swap_nodes/main.py
@@ -3,7 +3,24 @@
 #  two-dimensional array where each element is an array of integers representing
 #  the node indices of an in-order traversal after a swap operation.
 
+# Input
+#   - indexes: an array of integers representing index values of each node[i], beginning with node[1] , the first element, as the root.
+#   - queries: an array of integers, each representing a k value.
+
+# Output
+#   - For each k, perform the swap operation and store the indices of your in-order
+#   traversal to your result array. After all swap operations have been performed,
+#   return your result array for printing.
+
+
+# Note
+#   - -1 represents a null node
+
+
+
 def swapNodes(indexes, queries):
-    #
-    # Write your code here.
-    #
+    # construct trees
+
+    # swap
+
+    # print output
