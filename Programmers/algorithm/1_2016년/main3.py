006cd4fa6e330d0881f803eb4b2b17b602f9d6c1
Corgi <hwanggumedia@gmail.com>
2021-02-06 05:48:58 +0900

    feat: Add progres #2 '2016년, Programmers'

--- Programmers/algorithm/1_2016년/main3.py
+++ Programmers/algorithm/1_2016년/main3.py
@@ -3,5 +3,9 @@ import date
 def solution(a, b):
     answer = ''
 
+    # convert date from format '2016-a-b' to 'Monday', 'Tuesday' ...
+
+
+    # shorten to string of length 3
 
     return answer
