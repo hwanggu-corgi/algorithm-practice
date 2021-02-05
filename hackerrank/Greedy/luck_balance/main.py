1e60cd84a7b507e3075bb72d9524be842a37d68d
Corgi <hwanggumedia@gmail.com>
2021-02-06 05:03:50 +0900

    feat: Add solution 'Luck Balance, HackerRank'

--- hackerrank/Greedy/luck_balance/main.py
+++ hackerrank/Greedy/luck_balance/main.py
@@ -11,19 +11,20 @@ def luckBalance(k, contests):
     important_list = []
     unimportant_list = []
 
+    # order important by luck score
+    contests.sort(key= lambda e: e[0])
+
+    must_win_count = len([x[0] for x in contests if x[1] == 1]) - k
+
     # Separate important from unimportant
     for contest in contests:
-        if contest[1] == 1:
-            important_list.append(contest[0])
+        if contest[1] == 0:
+            answer += contest[0]
+        elif must_win_count > 0:
+            answer -= contest[0]
+            must_win_count -= 1
         else:
-            unimportant_list.append(contest[0])
-
-    # order important by luck score
-    important_list.sort()
-
-    # evaluate net amount
-    must_win_amt = len(important_list) - k
-    answer = sum(important_list[must_win_amt:]) - sum(important_list[:must_win_amt]) + sum(unimportant_list)
+            answer += contest[0]
 
     return answer
 
@@ -32,4 +33,5 @@ if __name__ == "__main__":
     print(luckBalance(4, [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]])) #31
     print(luckBalance(0, [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]])) #-1s
     print(luckBalance(0, [[5, 1]])) #-5
-    print(luckBalance(1, [[5, 1]])) #-5
+    print(luckBalance(1, [[5, 1]])) #5
+    print(luckBalance(5, [[5, 1]])) #5
