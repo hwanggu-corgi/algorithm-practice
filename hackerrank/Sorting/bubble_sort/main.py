5c786f6e2a53a55df3dff899d500ffb2a7fc19f4
Corgi <hwanggumedia@gmail.com>
2021-02-06 01:18:36 +0900

    feat: Add progress 'Bubble Sort, HackerRank'

---
+++ hackerrank/Sorting/bubble_sort/main.py
@@ -0,0 +1,14 @@
+# Given an array of integers, sort the array in ascending order using the Bubble
+# Sort algorithm above. Once sorted, print the following three lines:
+
+# Complete the countSwaps function below.
+
+
+def countSwaps(a):
+
+
+
+    print("Array is sorted in {} swaps.".format(count))
+    print("First Element: {}".format(a[0]))
+    print("Last Element: {}".format(a[-1]))
+
