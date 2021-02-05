e6cc592720c91a7e7a91951518ecb4839c2bde75
Corgi <hwanggumedia@gmail.com>
2021-02-06 04:45:46 +0900

    feat: Add progress #3 'Luck Balance, HackerRank'

--- hackerrank/Greedy/luck_balance/main.py
+++ hackerrank/Greedy/luck_balance/main.py
@@ -14,17 +14,22 @@ def luckBalance(k, contests):
     # Separate important from unimportant
     for contest in contests:
         if contest[1] == 1:
-            important_list.push(contest[0])
+            important_list.append(contest[0])
         else:
-            unimportant_list.push(contest[0])
+            unimportant_list.append(contest[0])
 
     # order important by luck score
     important_list.sort()
 
-    # Find the maximum luck possible
-    answer = sum(important_list) + sum(unimportant_list)
-
     # evaluate net amount
-    can_lose_amt =
+    must_win_amt = len(important_list) - k
+    answer = sum(important_list[must_win_amt:]) - sum(important_list[:must_win_amt]) + sum(unimportant_list)
 
     return answer
+
+if __name__ == "__main__":
+    print(luckBalance(3, [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]])) #29
+    print(luckBalance(4, [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]])) #31
+    print(luckBalance(0, [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]])) #-1s
+    print(luckBalance(0, [[5, 1]])) #-5
+    print(luckBalance(1, [[5, 1]])) #-5
