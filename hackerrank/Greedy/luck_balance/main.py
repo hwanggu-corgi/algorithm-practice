e92c29068392750db1096eb7ad0f67842b722556
Corgi <hwanggumedia@gmail.com>
2021-02-06 03:58:29 +0900

    feat: Add progress 'Luck Balance, HackerRank'

---
+++ hackerrank/Greedy/luck_balance/main.py
@@ -0,0 +1,21 @@
+# If Lena loses no more than k important contests, what is the maximum amount of
+# luck she can have after competing in all the preliminary contests? This value
+# may be negative.
+
+
+#constraints
+
+
+def luckBalance(k, contests):
+    answer = 0
+
+    # Separate important from unimportant
+
+    # order important by luck score
+
+    # Find the maximum luck possible
+
+    # evaluate net amount
+
+
+    return answer
