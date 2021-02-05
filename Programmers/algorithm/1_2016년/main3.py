b0b1349990fb03f63b8e3e2317dd83afa4f99d37
Corgi <hwanggumedia@gmail.com>
2021-02-06 06:02:00 +0900

    feat: Add solution '2016년, Programmers'

--- Programmers/algorithm/1_2016년/main3.py
+++ Programmers/algorithm/1_2016년/main3.py
@@ -1,11 +1,15 @@
-import date
+import datetime
 
 def solution(a, b):
     answer = ''
 
     # convert date from format '2016-a-b' to 'Monday', 'Tuesday' ...
-
+    answer = datetime.datetime.strptime('2016-{}-{}'.format(a,b), '%Y-%m-%d').strftime('%A')
 
     # shorten to string of length 3
+    answer = answer[:3].upper()
 
     return answer
+
+if __name__ == "__main__":
+    print(solution(5, 24)) #TUE
