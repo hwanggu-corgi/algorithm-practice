8c0f0c4d23316f969a8fe0c9a440e89aa788bfe2
Corgi <hwanggumedia@gmail.com>
2021-02-06 14:00:35 +0900

    feat: Add solution '문자열 내림차순으로 배치하기, Programmers'

--- Programmers/algorithm/1_문자열_내림차순으로_배치하기/main.py
+++ Programmers/algorithm/1_문자열_내림차순으로_배치하기/main.py
@@ -5,7 +5,10 @@
 #   str은 길이 1 이상인 문자열입니다.
 
 def solution(s):
-    answer = ''
-
-
+    lowercase_s = sorted([x for x in s if x.islower()], reverse=True)
+    uppercase_s = sorted([x for x in s if x.isupper()], reverse=True)
+    answer = ''.join(lowercase_s + uppercase_s)
     return answer
+
+if __name__ == "__main__":
+    print(solution("Zbcdefg"))
