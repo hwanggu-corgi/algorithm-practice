9eb96754d1b4452bf2a15412bc904c5d8eaf01a9
Corgi <hwanggumedia@gmail.com>
2021-02-07 07:20:21 +0900

    feat: Add progress #3 '전화번호 목록, Programmers'

--- Programmers/algorithm/2_전화번호 목록/main2.py
+++ Programmers/algorithm/2_전화번호 목록/main2.py
@@ -7,12 +7,21 @@
 #   각 전화번호의 길이는 1 이상 20 이하입니다.
 
 def solution(phone_book):
-    answer = True
-
     # for each number in phone_book
-    # find smaller phone number
-    # find larger phone number
-    # check if smaller is in larger phone number
-    # if yes, then break and return false
-    # if all is well, then return true
-    return answer
+    n = len(phone_book)
+    i = 0
+    while i < n:
+        j = i + 1
+        while j < n:
+            smaller = min(phone_book)
+            larger =  phone_book[j] if smaller == phone_book[i] else phone_book[i]
+            if smaller in larger:
+                return False
+            j += 1
+        i += 1
+    return True
+
+if __name__ == "__main__":
+    print(solution(["119", "97674223", "1195524421"])) #False
+    print(solution(["123", "456", "789"])) #True
+    print(solution(["97674223", "119", "1195524421"])) #False
