4d286880e40d1f09ac8e9443b95f22c0978da111
Corgi <hwanggumedia@gmail.com>
2021-02-06 09:00:16 +0900

    feat: Add solution 'Balanced Brackets, HackerRank'

--- hackerrank/Stack and Queues/balanced_brackets/main.py
+++ hackerrank/Stack and Queues/balanced_brackets/main.py
@@ -3,3 +3,40 @@
 
 # Complete the isBalanced function below.
 def isBalanced(s):
+    stack = []
+
+    for bracket in s:
+        # if it's left bracket then append to array
+        if bracket in ['[', '(', '{']:
+            stack.append(bracket)
+
+        # if it's right bracket then pop from array
+        else:
+            if len(stack) == 0:
+                return "NO"
+
+            left_bracket = stack.pop()
+
+            # check if popped bracket matches the right bracket
+            if ((bracket == ']' and left_bracket == '[') or
+                (bracket == ')' and left_bracket == '(') or
+                (bracket == '}' and left_bracket == '{')):
+
+                continue
+
+            # if not match, then return NO
+            else:
+                return "NO"
+
+
+    # if array is non-empty, return NO
+    if len(stack) > 0:
+        return "NO"
+
+    # if all is well, return YES
+    return "YES"
+
+if __name__ == "__main__":
+    print(isBalanced("{[()]}")) # YES
+    print(isBalanced("{[()]")) # NO
+    print(isBalanced("{{[[(())]]}}")) # YES
