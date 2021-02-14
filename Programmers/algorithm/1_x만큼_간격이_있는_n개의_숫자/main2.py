d0c9df76932d655c1e1c69f09fd419b35d907a1e
Corgi <hwanggumedia@gmail.com>
2021-02-15 05:17:36 +0900

    feat: Add solution 'x만큼 간격이 있는 n개의 숫자, Programmers'

---
+++ Programmers/algorithm/1_x만큼_간격이_있는_n개의_숫자/main2.py
@@ -0,0 +1,16 @@
+# goal: 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는
+# 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+# 제한 조건
+#   x는 -10000000 이상, 10000000 이하인 정수입니다.
+#   n은 1000 이하인 자연수입니다.
+
+def solution(x, n):
+    answer = []
+
+    for i in range(1, n+1):
+        answer.append(i * x)
+    return answer
+
+if __name__ == "__main__":
+    print(solution(2, 5)) #[2,4,6,8,10]
