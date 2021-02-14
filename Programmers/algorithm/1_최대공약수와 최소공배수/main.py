9cdc3ea091bfbe4fdff42475adeb018792e23176
Corgi <hwanggumedia@gmail.com>
2021-02-15 01:55:45 +0900

    feat: Add progress '최대공약수와 최소공배수, Programmers'

---
+++ Programmers/algorithm/1_최대공약수와 최소공배수/main.py
@@ -0,0 +1,12 @@
+def solution(n, m):
+    answer = []
+    return answer
+
+def gcd(a,b):
+    if a % b == 0:
+        return b
+    else
+        gcd(b, a % b)
+
+def lcm(a,b):
+    return a * b / gcd(a,b)
