60b32eaa9bb6928161dad076c90e907273f0dba1
Corgi <hwanggumedia@gmail.com>
2021-02-15 02:27:00 +0900

    fat: Add solution '수박수박수박수박수박수, Programmers'

--- Programmers/algorithm/1_수박수박수박수박수박수?/main.py
+++ Programmers/algorithm/1_수박수박수박수박수박수?/main.py
@@ -1,3 +1,14 @@
 def solution(n):
-    answer = ''
+    length = 1
+    answer = ""
+    while length <= n:
+        if length % 2 == 1:
+            answer += "수"
+        else:
+            answer += "박"
+        length += 1
     return answer
+
+if __name__ == "__main__":
+    print(solution(3)) #수박수
+    print(solution(4)) #수박수박
