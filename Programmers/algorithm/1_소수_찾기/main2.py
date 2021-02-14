8f100343e066f0ef1db95daa72c82be448f41b63
Corgi <hwanggumedia@gmail.com>
2021-02-15 04:59:47 +0900

    feat: Add solution '소수찾기, Programmers'

--- Programmers/algorithm/1_소수_찾기/main2.py
+++ Programmers/algorithm/1_소수_찾기/main2.py
@@ -1,5 +1,24 @@
 # goal: 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
 
 def solution(n):
-    answer = 0
+    answer = sieve(n).count(True)
     return answer
+
+def sieve(n):
+    sieve = [True] * (n+1)
+    sieve[0] = sieve[1] = False
+    i = 2
+
+    while (i * i <= n):
+        if (sieve[i]):
+            k = i * i
+        while (k <= n):
+            sieve[k] = False
+            k += i
+        i += 1
+
+    return sieve
+
+if __name__ == "__main__":
+    print(solution(10)) #4
+    print(solution(5)) #3
