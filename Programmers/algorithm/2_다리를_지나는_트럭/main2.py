6d2d9e8daa582d6893215aa6f5d3204d28ecf6b4
Corgi <hwanggumedia@gmail.com>
2021-02-15 13:51:50 +0900

    feat: Add progress '다리를 지나는 트럭, Programmers'

---
+++ Programmers/algorithm/2_다리를_지나는_트럭/main2.py
@@ -0,0 +1,7 @@
+# goal: solution 함수의 매개변수로 다리 길이 bridge_length, 다리가 견딜 수 있는 무게 weight,
+# 트럭별 무게 truck_weights가 주어집니다. 이때 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지
+# return 하도록 solution 함수를 완성하세요
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
