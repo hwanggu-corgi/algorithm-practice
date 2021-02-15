211268c03b71dee7bfbc9588fdd2c33c25d2fddd
Corgi <hwanggumedia@gmail.com>
2021-02-16 04:11:47 +0900

    feat: Add progress #2 '큰 수 만들기, Programmers'

--- Programmers/algorithm/2_큰_수_만들기/main2.py
+++ Programmers/algorithm/2_큰_수_만들기/main2.py
@@ -2,6 +2,10 @@
 # number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return
 # 하도록 solution 함수를 완성하세요.
 
+# 제한 조건
+#   number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
+#   k는 1 이상 number의 자릿수 미만인 자연수입니다.
+
 def solution(number, k):
     answer = ''
     return answer
