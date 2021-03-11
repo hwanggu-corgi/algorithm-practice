690269581f1f2eb45ad3ef69e3f165166f8b2f5f
Corgi <hwanggumedia@gmail.com>
2021-03-12 02:24:26 +0900

    feat: Add progress #2 '체육복, Programmers'

--- Programmers/algorithm/1_채육복/solution2.js
+++ Programmers/algorithm/1_채육복/solution2.js
@@ -2,14 +2,21 @@ function solution(n, lost, reserve) {
     var answer = 0;
 
     // create set lost and reserve with value not in both sets
-    let lost_set = new Set(lost);
-    let reserve_set = new Set(reserve);
+    let lost_set_tmp = new Set(lost);
+    let reserve_set_tmp = new Set(reserve);
 
-    lost_set = new Set([...lost].filter(x => reserve_set.has(x)));
-    lost_set = new Set([...lost].filter(x => reserve_set.has(x)));
+    let lost_set = new Set(lost.filter(x => !reserve_set_tmp.has(x)));
+    let reserve_set = new Set(reserve.filter(x => !lost_set_tmp.has(x)));
 
     // assign reserve to lost
     // if match, then take out value from lost and reserve
+    for (let item of reserve_set) {
+        if (lost_set.has(item-1)) {
+            lost_set.delete(item-1);
+        } else if (lost_set.has(item+1)) {
+            lost_set.delete(item+1);
+        }
+    }
 
     // find total number of participants
     // n - number of lost
