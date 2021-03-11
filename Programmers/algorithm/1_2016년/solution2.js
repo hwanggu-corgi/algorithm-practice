2af6489f087ac0a27aa161bdaf4a74ca45891223
Corgi <hwanggumedia@gmail.com>
2021-03-12 00:40:12 +0900

    feat: Add solution '2016년, Programmers'

--- Programmers/algorithm/1_2016년/solution2.js
+++ Programmers/algorithm/1_2016년/solution2.js
@@ -1,11 +1,16 @@
 function solution(a, b) {
     let answer = '';
-    let day_of_week_ref = {1: "MON", 2: "TUE", 3:"WED", 4:"THU", 5:"FRI", 6:"SAT", 7:"SUN"};
+    const day_of_week_ref = {0:"SUN", 1: "MON", 2: "TUE", 3:"WED", 4:"THU", 5:"FRI", 6:"SAT"};
 
     // convert input to date format
+    const date = new Date(`2016-${a}-${b}`);
 
     // get day of week
+    answer = day_of_week_ref[date.getDay()];
 
     // print
     return answer;
 }
+
+
+console.log(solution(5,24));
