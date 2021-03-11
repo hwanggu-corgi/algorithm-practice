c7610bff42d8fc8ed848dd835697aac915cc152c
Corgi <hwanggumedia@gmail.com>
2021-03-11 14:03:58 +0900

    feat: Add progress '모의고사, Programmers'

---
+++ Programmers/algorithm/1_모의고사/solution2.js
@@ -0,0 +1,19 @@
+function solution(answers) {
+    var answer = [];
+    let student_1 = [1,2,3,4,5];
+    let student_2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    let student_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    // find number of correct answers for each student
+    for (let i = 0; i < answers.length; i++) {
+
+        // if correct, then raise score
+        if (answer === student_1[i % student_1.length]) {
+
+        }
+    }
+
+    // write who has the biggest score of all
+
+    return answer;
+}
