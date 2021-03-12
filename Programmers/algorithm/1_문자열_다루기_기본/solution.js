6c35164c0bd5759e7400d8b84fec8f59ee7e3f81
Corgi <hwanggumedia@gmail.com>
2021-03-12 09:48:04 +0900

    feat: Add solution '문자열 다루기 기본, Programmers'

--- Programmers/algorithm/1_문자열_다루기_기본/solution.js
+++ Programmers/algorithm/1_문자열_다루기_기본/solution.js
@@ -1,5 +1,10 @@
 function solution(s) {
-    var answer = true;
-    const found = s.match(/[^0-9]+/);
-    return answer;
+    if (s.length !== 4 && s.length !== 6) {
+        return false;
+    }
+    const found = s.match(/[^0-9]/);
+    return found && found.length > 0 ? false : true;
 }
+
+console.log(solution("a234")); // false
+console.log(solution("1234")); // true
